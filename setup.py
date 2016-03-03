1dd9a10ab6f2abd84ff77d3ea3df39f02dd27a44
Rail Aliiev <rail@mozilla.com>
2016-03-03 09:49:30 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="signingworker",
-    version="0.11",
+    version="0.12",
     description="TaskCluster Signing Worker",
     author="Mozilla Release Engineering",
     author_email="release+python@mozilla.com",
