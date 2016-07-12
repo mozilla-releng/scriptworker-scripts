2fc83d8d470805fe667f5e4096264d6d81f2d9e0
Aki Sasaki <aki@escapewindow.com>
2016-07-12 12:43:26 -0700

    green tests

--- signingscript/task.py
+++ signingscript/task.py
@@ -1,5 +1,3 @@
-from jose import jwt
-from jose.constants import ALGORITHMS
 import json
 import logging
 import scriptworker.client
@@ -22,15 +20,6 @@ def task_signing_formats(task):
     """Extract last part of signing format scope"""
     return [s.split(":")[-1] for s in task["scopes"] if
             s.startswith("project:releng:signing:format:")]
-
-
-def validate_signature(task_id, token, pub_key, algorithms=(ALGORITHMS.RS512, )):
-    claims = jwt.decode(token, pub_key, algorithms=algorithms)
-    if task_id != claims.get("taskId"):
-        raise TaskVerificationError(
-            "Task IDs do not match. Expected {}, got {}".format(task_id, claims.get("taskId"))
-        )
-    return claims
 
 
 def validate_task_schema(context):
