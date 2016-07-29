d64278297ab557551f285d9baff447ba4193d0aa
Francis Kang <fkang@mozilla.com>
2016-07-28 22:02:19 -0400

    Change dockerhub credentials to mozillabalrog

--- tc_build.sh
+++ tc_build.sh
@@ -8,7 +8,7 @@ password_url="taskcluster/secrets/v1/secret/repo:github.com/mozilla-releng/funsi
 artifact_url="taskcluster/queue/v1/task/${TASK_ID}/runs/${RUN_ID}/artifacts/public/docker-image-shasum256.txt"
 artifact_expiry=$(date -d "+1 year" -u +%FT%TZ)
 dockerhub_email=fkang@mozilla.com
-dockerhub_username=fkang
+dockerhub_username=mozillabalrog
 dockerhub_password=$(curl ${password_url} | python -c 'import json, sys; a = json.load(sys.stdin); print a["secret"]["dockerhub_password"]')
 
 
@@ -38,16 +38,16 @@ fi
 date_tag="${branch}-${date}"
 
 echo "Building Docker image"
-docker build -t fkang/funsize-balrogworker:${branch_tag} .
+docker build -t mozilla/balrogworker:${branch_tag} .
 echo "Tagging Docker image with date tag"
-docker tag fkang/funsize-balrogworker:${branch_tag} "fkang/funsize-balrogworker:${date_tag}"
+docker tag mozilla/balrogworker:${branch_tag} "mozilla/balrogworker:${date_tag}"
 echo "Logging into Dockerhub"
 docker login -e $dockerhub_email -u $dockerhub_username -p $dockerhub_password
 echo "Pushing Docker image"
-docker push fkang/funsize-balrogworker:${branch_tag}
-docker push fkang/funsize-balrogworker:${date_tag}
+docker push mozilla/balrogworker:${branch_tag}
+docker push mozilla/balrogworker:${date_tag}
 
-sha256=$(docker images --no-trunc fkang/funsize-balrogworker | grep "${date_tag}" | awk '/^fkang/ {print $3}')
+sha256=$(docker images --no-trunc mozilla/balrogworker | grep "${date_tag}" | awk '/^mozilla/ {print $3}')
 echo "SHA256 is ${sha256}, creating artifact for it"
 put_url=$(curl --retry 5 --retry-delay 5 --data "{\"storageType\": \"s3\", \"contentType\": \"text/plain\", \"expires\": \"${artifact_expiry}\"}" ${artifact_url} | python -c 'import json; import sys; print json.load(sys.stdin)["putUrl"]')
 curl --retry 5 --retry-delay 5 -X PUT -H "Content-Type: text/plain" --data "${sha256}" "${put_url}"
