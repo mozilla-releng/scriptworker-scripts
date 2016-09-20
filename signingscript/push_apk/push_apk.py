7ca9484210d529f39440a195c1ad29edac3ab39c
Johan Lorenzo <jlorenzo@mozilla.com>
2016-09-20 11:35:24 +0200

    Allow PushAPK to be configured from another package

--- signingscript/push_apk/push_apk.py
+++ signingscript/push_apk/push_apk.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
+import sys
 import argparse
 import logging
-
 
 from oauth2client import client
 
@@ -13,6 +13,15 @@ FORMAT = '%(asctime)s - %(filename)s - %(levelname)s - %(message)s'
 logging.basicConfig(format=FORMAT)
 logger = logging.getLogger(__file__)
 logger.setLevel(10)
+
+
+class WrongArgumentGiven(Exception):
+    pass
+
+
+class ArgumentParser(argparse.ArgumentParser):
+    def error(self, message):
+        raise WrongArgumentGiven(message)
 
 
 class PushAPK():
@@ -26,39 +35,55 @@ class PushAPK():
         'org.mozilla.firefox': 'release'
     }
 
-    def __init__(self):
-        self.config = self._get_config_from_argv()
+    parser = None
+
+    def __init__(self, config=None):
+        self.config = self._parse_config(config)
         self.translationMgmt = StoreL10n()
 
-    @staticmethod
-    def _get_config_from_argv():
-        parser = argparse.ArgumentParser(
+    @classmethod
+    def _parse_config(cls, config=None):
+        if cls.parser is None:
+            cls._init_parser()
+
+        args = None if config is None else PushAPK._convert_dict_into_args(config)
+        # Parses sys.argv if args is None
+        return cls.parser.parse_args(args)
+
+    @classmethod
+    def _init_parser(cls):
+        cls.parser = ArgumentParser(
             description="""Upload the apk of a Firefox app on Google play.
 
-Example for a beta upload:
-$ python push_apk.py --package-name org.mozilla.firefox_beta --track production \
---service-account foo@developer.gserviceaccount.com --credentials key.p12 \
---apk-x86=/path/to/fennec-XX.0bY.multi.android-i386.apk \
---apk-armv7-v15=/path/to/fennec-XX.0bY.multi.android-arm-v15.apk""",
+    Example for a beta upload:
+    $ python push_apk.py --package-name org.mozilla.firefox_beta --track production \
+    --service-account foo@developer.gserviceaccount.com --credentials key.p12 \
+    --apk-x86=/path/to/fennec-XX.0bY.multi.android-i386.apk \
+    --apk-armv7-v15=/path/to/fennec-XX.0bY.multi.android-arm-v15.apk""",
             formatter_class=argparse.RawDescriptionHelpFormatter,
         )
 
-        parser.add_argument('--package-name', choices=PushAPK.PACKAGE_NAME_VALUES.keys(),
-                            help='The Google play name of the app', required=True)
-        parser.add_argument('--track', choices=PushAPK.TRACK_VALUES,
-                            default='alpha',    # We are not using alpha but we default to it to avoid mistake
-                            help='Track on which to upload')
+        cls.parser.add_argument('--package-name', choices=PushAPK.PACKAGE_NAME_VALUES.keys(),
+                                help='The Google play name of the app', required=True)
+        cls.parser.add_argument('--track', choices=PushAPK.TRACK_VALUES,
+                                default='alpha',    # We are not using alpha but we default to it to avoid mistake
+                                help='Track on which to upload')
 
-        parser.add_argument('--service-account', help='The service account email', required=True)
-        parser.add_argument('--credentials', dest='google_play_credentials_file', type=argparse.FileType(mode='rb'),
-                            default='key.p12', help='The p12 authentication file')
+        cls.parser.add_argument('--service-account', help='The service account email', required=True)
+        cls.parser.add_argument('--credentials', dest='google_play_credentials_file', type=argparse.FileType(mode='rb'),
+                                default='key.p12', help='The p12 authentication file')
 
-        parser.add_argument('--apk-x86', dest='apk_file_x86', type=argparse.FileType(),
-                            help='The path to the x86 APK file', required=True)
-        parser.add_argument('--apk-armv7-v15', dest='apk_file_armv7_v15', type=argparse.FileType(),
-                            help='The path to the ARM v7 API v15 APK file', required=True)
+        cls.parser.add_argument('--apk-x86', dest='apk_file_x86', type=argparse.FileType(),
+                                help='The path to the x86 APK file', required=True)
+        cls.parser.add_argument('--apk-armv7-v15', dest='apk_file_armv7_v15', type=argparse.FileType(),
+                                help='The path to the ARM v7 API v15 APK file', required=True)
 
-        return parser.parse_args()
+    @staticmethod
+    def _convert_dict_into_args(dict_):
+        dash_dash_dict = {'--{}'.format(key.replace('_', '-')): value for key, value in dict_.items()}
+        flatten_args = [item for tuples in dash_dash_dict.items() for item in tuples]
+        logger.debug('dict_ converveted into these args: %s', flatten_args)
+        return flatten_args
 
     def upload_apks(self, service, apk_files):
         """ Upload the APK to google play
@@ -88,8 +113,8 @@ $ python push_apk.py --package-name org.mozilla.firefox_beta --track production 
                     packageName=self.config.package_name,
                     media_body=apk_file_name).execute()
                 logger.info('Version code %d has been uploaded. '
-                         'Filename "%s" edit_id %s' %
-                         (apk_response['versionCode'], apk_file_name, edit_id))
+                            'Filename "%s" edit_id %s' %
+                            (apk_response['versionCode'], apk_file_name, edit_id))
 
                 versions.append(apk_response['versionCode'])
 
@@ -100,7 +125,7 @@ $ python push_apk.py --package-name org.mozilla.firefox_beta --track production 
 
             except client.AccessTokenRefreshError:
                 logger.critical('The credentials have been revoked or expired,'
-                         'please re-run the application to re-authorize')
+                                'please re-run the application to re-authorize')
 
         # Set the track for all apk
         service.edits().tracks().update(
@@ -109,7 +134,7 @@ $ python push_apk.py --package-name org.mozilla.firefox_beta --track production 
             packageName=self.config.package_name,
             body={u'versionCodes': versions}).execute()
         logger.info('Application "%s" set to track "%s" for versions %s' %
-                 (self.config.package_name, self.config.track, versions))
+                    (self.config.package_name, self.config.track, versions))
 
         # Commit our changes
         commit_request = service.edits().commit(
@@ -128,7 +153,7 @@ $ python push_apk.py --package-name org.mozilla.firefox_beta --track production 
                 continue
             locale = self.translationMgmt.locale_mapping(locale)
             logger.info('Locale "%s" what\'s new has been updated to "%s"'
-                     % (locale, whatsnew))
+                        % (locale, whatsnew))
 
             listing_response = service.edits().apklistings().update(
                 editId=edit_id, packageName=self.config.package_name, language=locale,
@@ -146,7 +171,13 @@ $ python push_apk.py --package-name org.mozilla.firefox_beta --track production 
 
 def main(name=None):
     if name == '__main__':
-        push_apk = PushAPK()
-        push_apk.run()
+        try:
+            push_apk = PushAPK()
+            push_apk.run()
+        except WrongArgumentGiven as e:
+            PushAPK.parser.print_help(sys.stderr)
+            sys.stderr.write('{}: error: {}\n'.format(PushAPK.parser.prog, e))
+            sys.exit(2)
+
 
 main(name=__name__)
