f381321078aed7b51ae4ae3d992417ba93fe8244
Johan Lorenzo <jlorenzo@mozilla.com>
2016-09-21 17:50:23 +0200

    Make first test pass

--- signingscript/test/test_task.py
+++ signingscript/test/test_task.py
@@ -1,25 +1,38 @@
-import pytest
-from scriptworker.exceptions import ScriptWorkerTaskException
-from signingscript.task import task_signing_formats, task_cert_type
+import json
+import copy
+from signingscript.script import get_default_config
+from signingscript.task import validate_task_schema
+from scriptworker.context import Context
+
+valid_task = json.loads("""
+{
+  "provisionerId": "meh",
+  "workerType": "workertype",
+  "schedulerId": "task-graph-scheduler",
+  "taskGroupId": "some",
+  "routes": [],
+  "retries": 5,
+  "created": "2015-05-08T16:15:58.903Z",
+  "deadline": "2015-05-08T18:15:59.010Z",
+  "expires": "2016-05-08T18:15:59.010Z",
+  "dependencies": ["VALID_TASK_ID"],
+  "scopes": ["signing"],
+  "payload": {
+    "apks": {
+      "armv7_v15": "https://queue.taskcluster.net/v1/task/DIYnEVJ_SaSLGWtd3_n3VA/artifacts/public%2Fbuild%2Ffennec-46.0a2.en-US.android-arm.apk",
+      "x86": "https://queue.taskcluster.net/v1/task/EZJ0suL7St65V_MM0iBhKw/artifacts/public%2Fbuild%2Ffennec-46.0a2.en-US.android-i386.apk"
+    },
+    "google_play_track": "alpha"
+  }
+}
+""")
+
+no_scopes = copy.deepcopy(valid_task)
+no_scopes["scopes"] = []
 
 
-def test_task_signing_formats():
-    task = {"scopes": ["project:releng:signing:cert:dep",
-                       "project:releng:signing:format:mar",
-                       "project:releng:signing:format:gpg"]}
-    assert ["mar", "gpg"] == task_signing_formats(task)
-
-
-def test_task_cert_type():
-    task = {"scopes": ["project:releng:signing:cert:dep",
-                       "project:releng:signing:type:mar",
-                       "project:releng:signing:type:gpg"]}
-    assert "project:releng:signing:cert:dep" == task_cert_type(task)
-
-
-def test_task_cert_type_error():
-    task = {"scopes": ["project:releng:signing:cert:dep",
-                       "project:releng:signing:cert:notdep",
-                       "project:releng:signing:type:gpg"]}
-    with pytest.raises(ScriptWorkerTaskException):
-        task_cert_type(task)
+def test_validate_task():
+    context = Context()
+    context.task = valid_task
+    context.config = get_default_config()
+    validate_task_schema(context)
--- signingscript/test/test_utils.py
+++
@@ -1,5 +0,0 @@
-from signingscript.utils import get_detached_signatures
-
-
-def test_detached_signatures():
-    assert get_detached_signatures(["mar", "gpg", "pgp"]) == [("gpg", ".asc", "text/plain")]
