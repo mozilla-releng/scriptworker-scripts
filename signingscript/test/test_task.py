f2c046f23cf05d30b8ee73347b1f57ec386b30e7
Johan Lorenzo <jlorenzo@mozilla.com>
2016-09-22 18:33:56 +0200

    Add async unit tests to task

--- signingscript/test/test_task.py
+++ signingscript/test/test_task.py
@@ -1,7 +1,9 @@
 import unittest
+import asynctest
 
 from signingscript.script import get_default_config
-from signingscript.task import validate_task_schema
+from signingscript.task import validate_task_schema, download_files
+from signingscript.exceptions import DownloadError
 
 from scriptworker.context import Context
 from scriptworker.exceptions import ScriptWorkerTaskException
@@ -32,3 +34,28 @@ class TaskTest(unittest.TestCase):
         ).generate_json()
 
         validate_task_schema(self.context)
+
+
+class TaskTestAsync(asynctest.TestCase):
+    def setUp(self):
+        self.context = Context()
+        self.context.config = get_default_config()
+        self.context.task = TaskGenerator().generate_json()
+
+    @asynctest.patch('signingscript.utils.download_file')
+    async def test_download_files_returns_absolute_paths(self, _):
+        files = await download_files(self.context)
+        path_prefix = '{}/public/build/fennec-46.0a2.en-US.android'.format(self.context.config['work_dir'])
+        self.assertEqual(files, {
+            'armv7_v15': '{}-arm.apk'.format(path_prefix),
+            'x86': '{}-i386.apk'.format(path_prefix),
+        })
+
+    @asynctest.patch('signingscript.utils.download_file')
+    async def test_download_files_raises_download_error(self, download_file):
+        def download_error(*args):
+            raise DownloadError("Not 200!")
+
+        download_file.side_effect = download_error
+        with self.assertRaises(DownloadError):
+            await download_files(self.context)
