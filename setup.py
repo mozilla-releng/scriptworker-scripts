89401c2497e98200f457cf4233ae5d997c86f07d
Johan Lorenzo <jlorenzo@mozilla.com>
2016-09-28 17:19:54 +0200

    Use published version of mozapkpublisher

--- setup.py
+++ setup.py
@@ -1,8 +1,19 @@
 import os
 from setuptools import setup, find_packages
 
-with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'version.txt')) as f:
+
+project_dir = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(project_dir, 'version.txt')) as f:
     version = f.read().rstrip()
+
+# We're using a pip8 style requirements file, which allows us to embed hashes
+# of the packages in it. However, setuptools doesn't support parsing this type
+# of file, so we need to strip those out before passing the requirements along
+# to it.
+with open(os.path.join(project_dir, 'requirements.txt')) as f:
+    requirements = [line.split()[0] for line in f if not line.startswith('#')]
+
 
 setup(
     name='pushapkworker',
@@ -21,11 +32,5 @@ setup(
         ],
     },
     license='MPL2',
-    install_requires=[
-        'arrow',
-        'python-jose',
-        'scriptworker',
-        'signtool',
-        'taskcluster',
-    ],
+    install_requires=requirements,
 )
