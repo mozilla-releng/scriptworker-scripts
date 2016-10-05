4c821608ef60b454926f98f7fad7e3a06eebfe93
Johan Lorenzo <jlorenzo@mozilla.com>
2016-10-05 17:37:11 +0200

    Use pytest.fixture

--- signingscript/test/test_task.py
+++ signingscript/test/test_task.py
@@ -31,17 +31,20 @@ def test_task_cert_type_error():
         task_cert_type(task)
 
 
-def test_missing_mandatory_urls_are_reported():
+@pytest.fixture
+def context():
     context = Context()
     context.config = get_default_config()
+    return context
+
+
+def test_missing_mandatory_urls_are_reported(context):
     context.task = task_generator.generate_object(urls=[])  # no URLs provided
 
     with pytest.raises(ScriptWorkerTaskException):
         validate_task_schema(context)
 
 
-def test_no_error_is_reported_when_no_missing_url():
-    context = Context()
-    context.config = get_default_config()
+def test_no_error_is_reported_when_no_missing_url(context):
     context.task = task_generator.generate_object()
     validate_task_schema(context)
