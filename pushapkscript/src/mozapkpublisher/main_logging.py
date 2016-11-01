7059d02493cbd8fb2409a18e0f441d4e1eecbe34
Johan Lorenzo <jlorenzo@mozilla.com>
2016-11-01 14:44:23 +0100

    [mozapkpublisher] Mute 3rd-party logs

--- pushapkscript/src/mozapkpublisher/main_logging.py
+++ pushapkscript/src/mozapkpublisher/main_logging.py
@@ -4,3 +4,4 @@ import logging
 def init():
     FORMAT = '%(asctime)s - %(filename)s - %(levelname)s - %(message)s'
     logging.basicConfig(format=FORMAT, level=logging.DEBUG)
+    logging.getLogger('oauth2client').setLevel(logging.WARNING)
