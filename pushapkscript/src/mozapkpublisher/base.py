e27f11f23bd0d96983281e385e8997dbe3b46b86
Johan Lorenzo <jlorenzo@mozilla.com>
2016-11-03 15:40:55 +0100

    [mozapkpublisher] Make _convert_dict_into_args() support unary options

--- pushapkscript/src/mozapkpublisher/base.py
+++ pushapkscript/src/mozapkpublisher/base.py
@@ -17,7 +17,20 @@ class Base(object):
 
     @staticmethod
     def _convert_dict_into_args(dict_):
-        dash_dash_dict = {'--{}'.format(key.replace('_', '-')): value for key, value in dict_.items()}
-        flatten_args = [item for tuples in dash_dash_dict.items() for item in tuples]
+        # For instance "dry_run" being True means the argument should be added to the command line.
+        dict_without_deactivated_unary_arguments = {key: value for key, value in dict_.items() if value is not False}
+
+        dash_dash_dict = {
+            '--{}'.format(key.replace('_', '-')): value
+            for key, value in dict_without_deactivated_unary_arguments.items()
+        }
+
+        args_with_unary_arguments_alone = [
+            (key, value) if not isinstance(value, bool) else (key,)
+            for key, value in dash_dash_dict.items()
+        ]
+
+        flatten_args = [item for tuples in args_with_unary_arguments_alone for item in tuples]
+
         logger.debug('dict_ converveted into these args: %s', flatten_args)
         return flatten_args
