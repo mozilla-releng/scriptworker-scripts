82086c289201657ef235ade72e6391579d718792
Aki Sasaki <aki@escapewindow.com>
2016-12-07 12:45:48 -1000

    use a config file!

--- balrogscript/balrogscript.py
+++ balrogscript/balrogscript.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 import os
 import logging
-import argparse
+from frozendict import frozendict
 import json
 import jsonschema
 import sys
 import hashlib
 import requests
-from copy import deepcopy
 import tempfile
 from boto.s3.connection import S3Connection
 from mardor.marfile import MarFile
@@ -58,19 +57,19 @@ def verify_signature(mar, signature):
     m.verify_signatures()
 
 
-def verify_copy_to_s3(args, mar_url, mar_dest):
+def verify_copy_to_s3(config, mar_url, mar_dest):
     # For local development, send TC url directly
-    if args.disable_s3:
+    if config['disable_s3']:
         return mar_url
 
-    conn = S3Connection(args.aws_key_id, args.aws_key_secret)
-    bucket = conn.get_bucket(args.s3_bucket)
+    conn = S3Connection(config['aws_key_id'], config['aws_key_secret'])
+    bucket = conn.get_bucket(config['s3_bucket'])
     _, dest = tempfile.mkstemp()
     log.info("Downloading %s to %s...", mar_url, dest)
     download(mar_url, dest)
     log.info("Verifying the signature...")
-    if not args.disable_certs:
-        verify_signature(dest, args.signing_cert)
+    if not config['disable_certs']:
+        verify_signature(dest, config['signing_cert'])
 
     for name in possible_names(mar_dest, 10):
         log.info("Checking if %s already exists", name)
@@ -112,19 +111,23 @@ def possible_names(initial_name, amount):
                              range(1, amount + 1)]
 
 
-@retriable()
-def get_manifest(parent_url):
-    manifest_url = parent_url + '/manifest.json'
-    log.info("Downloading manifest file from parent: %s" % manifest_url)
-    data = requests.get(manifest_url).content.decode('utf-8')
-    log.debug("Parsing returned manifest file")
-    manifest = json.loads(data)
+def get_manifest(config):
+    # assumes a single upstreamArtifact and single path
+    task_id = config['upstream_artifacts'][0]['taskId']
+    path = os.path.join(config['work_dir'], "cot", task_id, config['upstream_artifacts'][0]['paths'][0])
+    log.info("Reading manifest file %s" % path)
+    try:
+        with open(path, "r") as fh:
+            manifest = json.load(fh)
+    except (ValueError, OSError) as e:
+        log.critical("Can't load manifest from {}!\n{}".format(path, e))
+        sys.exit(3)
     return manifest
 
 
-def verify_task_schema(task_definition):
+def verify_task_schema(config, task_definition):
     schema_path = os.path.join(
-        os.path.dirname(__file__), "data", "balrog_task_schema.json"
+        os.path.dirname(os.path.dirname(__file__)), config['schema_file']
     )
     with open(schema_path) as fh:
         schema = json.load(fh)
@@ -136,19 +139,25 @@ def verify_task_schema(task_definition):
         sys.exit(3)
 
 
-def load_task(task_file):
+def load_task(config, task_file):
     with open(task_file, 'r') as f:
         task_definition = json.load(f)
 
-    verify_task_schema(task_definition)
-    parent_url = task_definition['payload']['parent_task_artifacts_url']
-    signing_cert_name = task_definition['payload']['signing_cert']
+    verify_task_schema(config, task_definition)
+    upstream_artifacts = task_definition['payload']['upstreamArtifacts']
+    for scope in task_definition['scopes']:
+        if scope.startswith("project:releng:balrog:"):
+            signing_cert_name = scope.split(':')[-1]
+            break
+    else:
+        log.critical("no balrog scopes!")
+        sys.exit(3)
     bin_directory = os.path.dirname(os.path.abspath(__file__))
     signing_cert = os.path.join(bin_directory, "keys/{}.pubkey".format(signing_cert_name))
-    return parent_url, signing_cert
+    return upstream_artifacts, signing_cert
 
 
-def create_submitter(e, balrog_auth, args):
+def create_submitter(e, balrog_auth, config):
     auth = balrog_auth
 
     if "previousVersion" in e and "previousBuildNumber" in e:
@@ -164,9 +173,9 @@ def create_submitter(e, balrog_auth, args):
         }]
         partial_info[0]["previousVersion"] = e["previousVersion"]
         partial_info[0]["previousBuildNumber"] = e["previousBuildNumber"]
-        submitter = ReleaseSubmitterV4(api_root=args.api_root,
+        submitter = ReleaseSubmitterV4(api_root=config['api_root'],
                                        auth=auth,
-                                       dummy=args.dummy)
+                                       dummy=config['dummy'])
 
         return submitter, {'platform': e["platform"],
                            'productName': e["appName"],
@@ -184,8 +193,8 @@ def create_submitter(e, balrog_auth, args):
         log.info("Taskcluster Nightly Fennec style Balrog submission")
 
         complete_info = e['completeInfo']
-        complete_info[0]["url"] = verify_copy_to_s3(args, complete_info[0]['url'], '')
-        submitter = NightlySubmitterV4(api_root=args.api_root, auth=auth, dummy=args.dummy,
+        complete_info[0]["url"] = verify_copy_to_s3(config, complete_info[0]['url'], '')
+        submitter = NightlySubmitterV4(api_root=config['api_root'], auth=auth, dummy=config['dummy'],
                                        url_replacements=e.get('url_replacements', []))
 
         return submitter, {'platform': e["platform"],
@@ -201,72 +210,63 @@ def create_submitter(e, balrog_auth, args):
         raise RuntimeError("Cannot determine Balrog submission style. Check manifest.json")
 
 
-def verify_args(argv):
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument("--taskdef", required=True,
-                        help="File location of task graph"),
-    parser.add_argument("-d", "--dummy", action="store_true",
-                        help="Add '-dummy' suffix to branch name")
-    parser.add_argument("-v", "--verbose", action="store_const",
-                        dest="loglevel", const=logging.DEBUG,
-                        default=logging.INFO)
-    parser.add_argument("--balrog-api-root", default=os.environ.get("BALROG_API_ROOT"),
-                        dest="api_root", help="Balrog api url")
-    parser.add_argument("--balrog-username", default=os.environ.get("BALROG_USERNAME"),
-                        dest="balrog_username", help="Balrog admin api username")
-    parser.add_argument("--balrog-password", default=os.environ.get("BALROG_PASSWORD"),
-                        dest="balrog_password", help="Balrog admin api password")
-    parser.add_argument("--s3-bucket", default=os.environ.get("S3_BUCKET"),
-                        dest="s3_bucket", help="S3 Bucket Name: used for uploading partials")
-    parser.add_argument("--aws-access-key-id", default=os.environ.get("AWS_ACCESS_KEY_ID"),
-                        dest="aws_key_id", help="AWS Access Key ID: S3 Credentials")
-    parser.add_argument("--aws-secret-access-key", default=os.environ.get("AWS_SECRET_ACCESS_KEY"),
-                        dest="aws_key_secret", help="AWS Secret Key: S3 Credentials")
-    parser.add_argument("--disable-s3", default=False, action='store_true',
-                        help="Instead of uploading artifacts to S3, send balrog the tc artifact url")
-    parser.add_argument("--disable-certs", dest="disable_certs", action="store_true",
-                        help="Disable mar signature verification")
-
-    args = parser.parse_args(argv)
+def get_config(argv):
+    try:
+        with open(argv[0]) as fh:
+            config = json.load(fh)
+    except (ValueError, OSError) as e:
+        log.critical("Can't read config file {}!\n{}".format(argv[0], e))
+        sys.exit(5)
+    except KeyError as e:
+        log.critical("Usage: balrogscript CONFIG_FILE\n{}".format(e))
+        sys.exit(5)
+    for config_key, env_var in {
+        "api_root": "BALROG_API_ROOT",
+        "balrog_username": "BALROG_USERNAME",
+        "balrog_password": "BALROG_PASSWORD",
+        "s3_bucket": "S3_BUCKET",
+        "aws_key_id": "AWS_ACCESS_KEY_ID",
+        "aws_key_secret": "AWS_SECRET_ACCESS_KEY",
+    }:
+        config.setdefault(config_key, os.environ.get(env_var))
+        if config[config_key] is None:
+            log.critical("{} missing from config! (You can also set the env var {})".format(config_key, env_var))
+            sys.exit(5)
 
     # Disable uploading to S3 if any of the credentials are missing, or if specified as a cli argument
-    args.disable_s3 = args.disable_s3 or not (args.s3_bucket and args.aws_key_id and args.aws_key_secret)
-    if args.disable_s3:
+    config['disable_s3'] = config['disable_s3'] or not (config['s3_bucket'] and config['aws_key_id'] and config['aws_key_secret'])
+    if config['disable_s3']:
         log.info("Skipping S3 uploads, submitting taskcluster artifact urls instead.")
 
-    args.parent_url, args.signing_cert = load_task(args.taskdef)
+    taskdef = os.path.join(config['work_dir'], "task.json")
+    config['upstream_artifacts'], config['signing_cert'] = load_task(config, taskdef)
 
-    return args
+    # get the balrog creds out of config
+    username, password = (config['balrog_username'], config['balrog_password'])
+    del(config['balrog_username'])
+    del(config['balrog_password'])
 
-
-def update_args_by_popping_balrog_creds(args):
-    """Pops balrog auth creds from args and retrieves them"""
-    username, password = (args.balrog_username, args.balrog_password)
-
-    new_args = deepcopy(args)
-    del(new_args.balrog_username)
-    del(new_args.balrog_password)
-
-    return ((username, password), new_args)
+    return (username, password), frozendict(config)
 
 
 def main():
-    args = verify_args(sys.argv[1:])
-    balrog_auth, args = update_args_by_popping_balrog_creds(args)
+    balrog_auth, config = get_config(sys.argv[1:])
+    level = logging.INFO
+    if config['verbose']:
+        level = logging.DEBUG
 
     logging.basicConfig(format="%(asctime)s - %(levelname)s - %(message)s",
                         stream=sys.stdout,
-                        level=args.loglevel)
+                        level=level)
     logging.getLogger("requests").setLevel(logging.WARNING)
     logging.getLogger("boto").setLevel(logging.WARNING)
 
     # Download the manifest.json from the provided task
-    manifest = get_manifest(args.parent_url)
+    manifest = get_manifest(config)
 
     for e in manifest:
         # Get release metadata from manifest, and upload to S3 if necessary
-        submitter, release = create_submitter(e, balrog_auth, args)
+        submitter, release = create_submitter(e, balrog_auth, config)
         # Connect to balrog and submit the metadata
         retry(lambda: submitter.run(**release))
 
