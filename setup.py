38924909327765fd8a2ba0d73ef9478c44434841
Johan Lorenzo <jlorenzo@mozilla.com>
2017-02-01 11:54:45 +0100

    Bug 1334425 - Only pin dependencies in Puppet (#12)

--- setup.py
+++ setup.py
@@ -7,10 +7,7 @@ project_dir = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(project_dir, 'version.txt')) as f:
     version = f.read().rstrip()
 
-# We're using a pip8 style requirements file, which allows us to embed hashes
-# of the packages in it. However, setuptools doesn't support parsing this type
-# of file, so we need to strip those out before passing the requirements along
-# to it.
+# We allow commented lines in this file
 with open(os.path.join(project_dir, 'requirements.txt')) as f:
     requirements = [line.split()[0] for line in f if not line.startswith('#')]
 
