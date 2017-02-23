ef9ea9b17e7ecbd67a1088919625ea12d2cbfc47
Mihai Tabara <mtabara@mozilla.com>
2017-02-23 16:01:25 +0200

    0.3.0-beta1

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
+import json
 import os
 from setuptools import setup, find_packages
 
-with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "version.txt")) as f:
-    version = f.read().rstrip()
+PATH = os.path.join(os.path.dirname(__file__), "version.json")
+with open(PATH) as filehandle:
+    VERSION = json.load(filehandle)['version_string']
 
 setup(
     name="beetmoverscript",
-    version=version,
+    version=VERSION,
     description="TaskCluster Beetmover Script",
     author="Mozilla Release Engineering",
     author_email="release+python@mozilla.com",
     url="https://github.com/mozilla-releng/beetmoverscript/",
     packages=find_packages(),
-    package_data={"beetmoverscript": ["data/*", "templates/*"]},
+    package_data={
+        "beetmoverscript": ["data/*", "templates/*"],
+        "": ["version.json"],
+    },
     include_package_data=True,
     zip_safe=False,
     entry_points={
@@ -29,4 +34,9 @@ setup(
         "PyYAML",
         "Jinja2",
     ],
+    classifiers=(
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.5',
+    ),
 )
