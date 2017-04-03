c49d4071e230fbf1473f0941d62d0b76307cc3f2
Johan Lorenzo <jlorenzo@mozilla.com>
2017-04-03 18:17:39 +0200

    [mozapkpublisher] base: add test to make sure integers are allowed by _parse_config

--- pushapkscript/src/mozapkpublisher/test/test_base.py
+++ pushapkscript/src/mozapkpublisher/test/test_base.py
@@ -10,6 +10,7 @@ class TestBase(Base):
         cls.parser = argparse.ArgumentParser(description='Test parser with dummy data')
         cls.parser.add_argument('--a-key')
         cls.parser.add_argument('--k')
+        cls.parser.add_argument('--an-int', type=int)
 
 
 def test_convert_dict_into_args():
@@ -26,13 +27,15 @@ def test_convert_dict_into_args():
 
 
 def test_parse_config():
-    config = TestBase._parse_config(config={'a_key': 'a_value', 'k': 'v'})
+    config = TestBase._parse_config(config={'a_key': 'a_value', 'k': 'v', 'an_int': 10})
     assert config.a_key == 'a_value'
     assert config.k == 'v'
+    assert config.an_int == 10
 
 
 def test_parse_config_from_argv(monkeypatch):
-    monkeypatch.setattr(sys, 'argv', ['testcall', '--a-key', 'a_value', '--k', 'v'])
+    monkeypatch.setattr(sys, 'argv', ['testcall', '--a-key', 'a_value', '--k', 'v', '--an-int', '10'])
     config = TestBase._parse_config()
     assert config.a_key == 'a_value'
     assert config.k == 'v'
+    assert config.an_int == 10
