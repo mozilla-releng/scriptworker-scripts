8e541373cb341573b62545c183c6f2fcd969a7d2
Ben Hearsum <bhearsum@mozilla.com>
2017-05-16 16:30:51 -0400

    Add old id fennec platforms. (#62)

--- beetmoverscript/constants.py
+++ beetmoverscript/constants.py
@@ -22,7 +22,9 @@ STAGE_PLATFORM_MAP = {
 # use explicit platform 'firefox' mapping when all desktop platforms are added
 TEMPLATE_KEY_PLATFORMS = defaultdict(lambda: "firefox", {
     "android-api-15": "fennec",
-    "android-x86": "fennecx86"
+    "android-x86": "fennecx86",
+    "android-api-15-old-id": "fennec",
+    "android-x86-old-id": "fennecx86",
 })
 HASH_BLOCK_SIZE = 1024*1024
 INITIAL_RELEASE_PROPS_FILE = "balrog_props.json"
