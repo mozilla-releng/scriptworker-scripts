0759499b8b755a499c1551a121f8b84cd590bb08
Johan Lorenzo <jlorenzo@mozilla.com>
2017-05-31 10:35:27 +0200

    Verify APK has a signature with SHA-1 digest (#13)

--- pushapkscript/jarsigner.py
+++ pushapkscript/jarsigner.py
@@ -1,9 +1,12 @@
 import logging
+import re
 import subprocess
 
 from pushapkscript.exceptions import SignatureError
 
 log = logging.getLogger(__name__)
+
+DIGEST_ALGORITHM_REGEX = re.compile(r'\s*Digest algorithm: (\S+)$', re.MULTILINE)
 
 
 def verify(context, apk_path, channel):
@@ -12,16 +15,38 @@ def verify(context, apk_path, channel):
 
     completed_process = subprocess.run([
         binary_path, '-verify', '-strict',
+        '-verbose',     # Needed to check the digest algorithm
         '-keystore', keystore_path,
         apk_path,
         certificate_alias
     ], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
 
-    if completed_process.returncode != 0:
-        log.critical(completed_process.stdout)
+    command_output = completed_process.stdout
+    _check_certificate_via_return_code(completed_process.returncode, command_output, binary_path, apk_path, certificate_alias, keystore_path)
+    _check_digest_algorithm(command_output)
+
+
+def _check_certificate_via_return_code(return_code, command_output, binary_path, apk_path, certificate_alias, keystore_path):
+    if return_code != 0:
+        log.critical(command_output)
         raise SignatureError(
             '{} doesn\'t verify apk "{}". It compared certificate against "{}", located in keystore "{}"'
             .format(binary_path, apk_path, certificate_alias, keystore_path)
+        )
+
+
+def _check_digest_algorithm(command_output):
+    # This prevents https://bugzilla.mozilla.org/show_bug.cgi?id=1332916
+    match_result = DIGEST_ALGORITHM_REGEX.search(command_output)
+    if match_result is None:
+        log.critical(command_output)
+        raise SignatureError('Could not find what digest algorithm was used to sign this APK')
+
+    digest_algorithm = match_result.group(1)
+    if digest_algorithm != 'SHA1':
+        log.critical(command_output)
+        raise SignatureError(
+            'Wrong digest algorithm: SHA1 digest is expected, but "{}" was found'.format(digest_algorithm)
         )
 
 
--- pushapkscript/test/test_jarsigner.py
+++ pushapkscript/test/test_jarsigner.py
@@ -31,26 +31,49 @@ class JarSignerTest(unittest.TestCase):
             with patch('subprocess.run') as run:
                 run.return_value = MagicMock()
                 run.return_value.returncode = 0
+                run.return_value.stdout = '''
+                    smk      632 Mon Feb 01 12:54:21 CET 2016 application.ini
+                        Digest algorithm: SHA1
+                '''
                 jarsigner.verify(self.context, '/path/to/apk', channel)
 
                 run.assert_called_with([
-                    '/path/to/jarsigner', '-verify', '-strict', '-keystore', '/path/to/keystore', '/path/to/apk', alias
+                    '/path/to/jarsigner', '-verify', '-strict', '-verbose', '-keystore', '/path/to/keystore', '/path/to/apk', alias
                 ], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
 
     def test_verify_should_call_executable_with_defaults_arguments(self):
         with patch('subprocess.run') as run:
             run.return_value = MagicMock()
             run.return_value.returncode = 0
+            run.return_value.stdout = 'Digest algorithm: SHA1'
             jarsigner.verify(self.minimal_context, '/path/to/apk', channel='aurora')
 
             run.assert_called_with([
-                'jarsigner', '-verify', '-strict', '-keystore', '/path/to/keystore', '/path/to/apk', 'nightly'
+                'jarsigner', '-verify', '-strict', '-verbose', '-keystore', '/path/to/keystore', '/path/to/apk', 'nightly'
             ], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
 
     def test_raises_error_when_return_code_is_not_0(self):
         with patch('subprocess.run') as run:
             run.return_value = MagicMock()
             run.return_value.returncode = 1
+
+            with self.assertRaises(SignatureError):
+                jarsigner.verify(self.context, '/path/to/apk', channel='aurora')
+
+    def test_raises_error_when_digest_is_not_sha1(self):
+        with patch('subprocess.run') as run:
+            run.return_value = MagicMock()
+            run.return_value.returncode = 0
+            run.return_value.stdout = 'Digest algorithm: SHA256'
+
+            with self.assertRaises(SignatureError):
+                jarsigner.verify(self.context, '/path/to/apk', channel='aurora')
+
+    def test_raises_error_when_no_digest_algo_is_returned_by_jarsigner(self):
+        with patch('subprocess.run') as run:
+            run.return_value = MagicMock()
+            run.return_value.returncode = 0
+            run.return_value.stdout = 'Some random output'
 
             with self.assertRaises(SignatureError):
                 jarsigner.verify(self.context, '/path/to/apk', channel='aurora')
