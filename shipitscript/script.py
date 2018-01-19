c127a91a62b82fd632a0c474194bbf78d97078c6
Johan Lorenzo <jlorenzo@mozilla.com>
2018-01-19 15:12:31 +0100

    Initial commit (Sample scritpworker project)

---
+++ shipitscript/script.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+""" ShipIt main script
+"""
+import aiohttp
+import asyncio
+import logging
+import os
+import sys
+import traceback
+
+import scriptworker.client
+from scriptworker.context import Context
+from scriptworker.exceptions import ScriptWorkerTaskException
+
+from shipitscript.utils import load_json
+
+
+log = logging.getLogger(__name__)
+
+
+async def async_main(context):
+    context.task = scriptworker.client.get_task(context.config)
+    log.info('Hello World!')
+
+
+def get_default_config():
+    cwd = os.getcwd()
+    parent_dir = os.path.dirname(cwd)
+
+    return {
+        'work_dir': os.path.join(parent_dir, 'work_dir'),
+        'schema_file': os.path.join(cwd, 'shipitscript', 'data', 'shipit_task_schema.json'),
+        'verbose': False,
+    }
+
+
+def usage():
+    print("Usage: {} CONFIG_FILE".format(sys.argv[0]), file=sys.stderr)
+    sys.exit(1)
+
+
+def main(name=None, config_path=None, close_loop=True):
+    if name not in (None, '__main__'):
+        return
+    context = Context()
+    context.config = get_default_config()
+    if config_path is None:
+        if len(sys.argv) != 2:
+            usage()
+        config_path = sys.argv[1]
+    context.config.update(load_json(path=config_path))
+
+    logging.basicConfig(**craft_logging_config(context))
+    logging.getLogger('taskcluster').setLevel(logging.WARNING)
+
+    loop = asyncio.get_event_loop()
+    with aiohttp.ClientSession() as session:
+        context.session = session
+        try:
+            loop.run_until_complete(async_main(context))
+        except ScriptWorkerTaskException as exc:
+            traceback.print_exc()
+            sys.exit(exc.exit_code)
+
+    if close_loop:
+        # Loop cannot be reopen once closed. Not closing it allows to run several tests on main()
+        loop.close()
+
+
+def craft_logging_config(context):
+    return {
+        'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        'level': logging.DEBUG if context.config.get('verbose') else logging.INFO
+    }
+
+
+main(name=__name__)
---
+++ shipitscript/test/test_utils.py
@@ -0,0 +1,16 @@
+import os
+import tempfile
+import json
+
+from shipitscript.utils import load_json
+
+
+def test_load_json_from_file():
+    json_object = {'a_key': 'a_value'}
+
+    with tempfile.TemporaryDirectory() as output_dir:
+        output_file = os.path.join(output_dir, 'file.json')
+        with open(output_file, 'w') as f:
+            json.dump(json_object, f)
+
+        assert load_json(output_file) == json_object
