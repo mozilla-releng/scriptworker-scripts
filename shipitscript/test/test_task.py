5ee5bb29e1959abea35ba6800e6d6c8b48fd6209
Johan Lorenzo <jlorenzo@mozilla.com>
2018-01-22 16:24:59 +0100

    Added minimal task schema

--- shipitscript/test/test_task.py
+++ shipitscript/test/test_task.py
@@ -7,5 +7,10 @@ from shipitscript.task import validate_task_schema
 def test_validate_task():
     context = Context()
     context.config = get_default_config()
-    context.task = {}
+    context.task = {
+        'dependencies': ['someTaskId'],
+        'payload': {
+            'release_name': 'Firefox-59.0b3-build1'
+        }
+    }
     validate_task_schema(context)
