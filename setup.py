c5deeb593d2f06929ac551a9fc2175afb27cc57d
Justin Wood <Callek@gmail.com>
2018-01-30 09:37:10 -0500

    Skeleton of required files, coverage travis changelog, etc.

---
+++ setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup, find_packages
+
+with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "version.txt")) as f:
+    version = f.read().rstrip()
+
+setup(
+    name="treescript",
+    version=version,
+    description="Tree Modifying Script",
+    author="Mozilla Release Engineering",
+    author_email="release+python@mozilla.com",
+    url="https://github.com/mozilla-releng/treescript",
+    packages=find_packages(),
+    # package_data={"signingscript": ["data/*"]}, # XXX: Todo
+    include_package_data=True,
+    zip_safe=False,
+    # entry_points={ # XXX: Todo
+    #    "console_scripts": [
+    #        "signingscript = signingscript.script:main",
+    #    ],
+    # },
+    license="MPL2",
+    install_requires=[  # XXX: Todo
+        # "arrow",
+        # "datadog",
+        # "python-jose",
+        "scriptworker",
+        # "taskcluster",
+    ],
+)
