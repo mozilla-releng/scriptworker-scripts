1cc382ae3af20f394db067c7bcb9a81b6d398d3a
Aki Sasaki <aki@escapewindow.com>
2018-02-22 18:06:24 +0000

    possibly py3 compatible

--- bouncerscript/utils.py
+++ bouncerscript/utils.py
@@ -1,16 +1,11 @@
+import aiohttp
 import json
 import logging
-# import base64
-# import httplib
-# import socket
-# import sys
-# import traceback
-# import urllib
-# import urllib2
+import sys
+import traceback
 from xml.dom.minidom import parseString
-from six.moves.urllib.parse import quote
-
-# from mozharness.base.log import FATAL
+from urllib.parse import quote
+from scriptworker.util import retry_async
 
 
 log = logging.getLogger(__name__)
@@ -25,9 +20,8 @@ async def api_call(context, route, data, error_level='fatal', retry_config=None)
     """TODO"""
     retry_async_kwargs = dict(
         failure_status=None,
-        retry_exceptions=(urllib2.HTTPError, urllib2.URLError,
-                          httplib.BadStatusLine,
-                          socket.timeout, socket.error),
+        retry_exceptions=(aiohttp.ClientError,
+                          aiohttp.ServerTimeoutError),
         error_message="call to %s failed" % (route),
         error_level=error_level,
     )
@@ -36,8 +30,8 @@ async def api_call(context, route, data, error_level='fatal', retry_config=None)
         retry_async_kwargs.update(retry_config)
 
     await retry_async(_api_call, args=(route, data),
-                      retry_exceptions=(Exception, ),
                       **retry_async_kwargs)
+
 
 async def _api_call(context, route, data):
     """TODO"""
@@ -47,39 +41,30 @@ async def _api_call(context, route, data):
     api_root = bouncer_config["api_root"]
     api_url = "%s/%s" % (api_root, route)
 
-    request = urllib2.Request(api_url)
+    kwargs = {'timeout': 60}
+    auth = None
     if data:
-        post_data = urllib.urlencode(data, doseq=True)
-        request.add_data(post_data)
-        self.info("POST data: %s" % post_data)
+        kwargs['json'] = data
     if credentials:
-        auth = base64.encodestring('%s:%s' % credentials)
-        request.add_header("Authorization", "Basic %s" % auth.strip())
-    try:
-        self.info("Submitting to %s" % api_url)
-        res = urllib2.urlopen(request, timeout=60).read()
-        self.info("Server response")
-        self.info(res)
-        return res
-    except urllib2.HTTPError as e:
-        self.warning("Cannot access %s" % api_url)
-        traceback.print_exc(file=sys.stdout)
-        self.warning("Returned page source:")
-        self.warning(e.read())
-        raise
-    except urllib2.URLError:
-        traceback.print_exc(file=sys.stdout)
-        self.warning("Cannot access %s" % api_url)
-        raise
-    except socket.timeout as e:
-        self.warning("Timed out accessing %s: %s" % (api_url, e))
-        raise
-    except socket.error as e:
-        self.warning("Socket error when accessing %s: %s" % (api_url, e))
-        raise
-    except httplib.BadStatusLine as e:
-        self.warning('BadStatusLine accessing %s: %s' % (api_url, e))
-        raise
+        # XXX This may need to be latin1
+        auth = aiohttp.BasicAuth(*credentials, encoding='utf-8')
+    async with aiohttp.ClientSession(auth=auth) as session:
+        log.info("Submitting to %s" % api_url)
+        try:
+            async with session.post(api_url, **kwargs) as resp:
+                log.info("Server response")
+                result = await resp.txt()
+                log.info(result)
+                return result
+        except aiohttp.ClientError as e:
+            log.warning("Cannot access %s" % api_url)
+            traceback.print_exc(file=sys.stdout)
+            log.warning("Returned page source:")
+            log.warning(e.read())
+            raise
+        except aiohttp.ServerTimeoutError as e:
+            log.warning("Timed out accessing %s: %s" % (api_url, e))
+            raise
 
 
 async def product_exists(context, product_name):
