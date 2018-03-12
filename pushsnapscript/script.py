8e1ff309d34a37da9837cbe1c90662864b134fa0
Johan Lorenzo <jlorenzo@mozilla.com>
2018-03-12 17:23:30 +0100

    Add artifacts.py

--- pushsnapscript/script.py
+++ pushsnapscript/script.py
@@ -3,10 +3,15 @@
 """
 from scriptworker import client
 
+from pushsnapscript import artifacts
+
 
 async def async_main(context):
     context.task = client.get_task(context.config)
-    print('Hello World!')
+
+    # TODO Sanity checks on the file
+    snap_file_path = artifacts.get_snap_file_path(context)
+    print(snap_file_path)
 
 
 __name__ == '__main__' and client.sync_main(async_main)
--- pushsnapscript/test/test_script.py
+++ pushsnapscript/test/test_script.py
@@ -3,16 +3,18 @@ import pytest
 from scriptworker import client
 from unittest.mock import MagicMock
 
+from pushsnapscript import artifacts
 from pushsnapscript.script import async_main
 
 
 @pytest.mark.asyncio
 async def test_hello_world(capsys, monkeypatch):
     monkeypatch.setattr(client, 'get_task', lambda _: {})
+    monkeypatch.setattr(artifacts, 'get_snap_file_path', lambda _: '/some/file.snap')
     context = MagicMock()
 
     await async_main(context)
 
     captured = capsys.readouterr()
-    assert captured.out == 'Hello World!\n'
+    assert captured.out == '/some/file.snap\n'
     assert captured.err == ''
