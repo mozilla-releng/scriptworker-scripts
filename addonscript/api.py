34f4ff2e6422c7410ee6e6157872efaedea225d3
Justin Wood <Callek@gmail.com>
2018-03-27 15:32:06 -0400

    Snapshot of work

---
+++ addonscript/api.py
@@ -0,0 +1,50 @@
+"""API helpers for addonscript."""
+
+from addonscript.utils import get_api_url, amo_put, amo_get, amo_download
+from addonscript.task import get_version, get_channel
+
+# https://addons-server.readthedocs.io/en/latest/topics/api/signing.html#uploading-a-version
+UPLOAD_VERSION = "api/v3/addons/{id}/versions/{version}/"
+
+# https://addons-server.readthedocs.io/en/latest/topics/api/signing.html#checking-the-status-of-your-upload
+UPLOAD_STATUS = "api/v3/addons/{id}/versions/{version}/upload/{upload_pk}/"
+
+
+async def do_upload(context, locale):
+    """Upload the language pack for `locale` to AMO.
+
+    Returns the JSON response from AMO
+    """
+    # XXX langpack_id may change for devedition
+    langpack_id = 'langpack-{}@firefox.mozilla.org'.format(locale)
+    version = get_version(context)
+    url = get_api_url(context, UPLOAD_VERSION, id=langpack_id, version=version)
+    with open(context.locales[locale]['unsigned'], 'rb') as file:
+        data = {
+            'channel': get_channel(context),
+            'upload': file,
+        }
+        return await amo_put(context, url, data)
+
+
+async def get_upload_status(context, locale, upload_pk):
+    """Query AMO for the status of a given upload for `locale`.
+
+    Returns the JSON response from AMO
+    """
+    # XXX langpack_id may change for devedition
+    langpack_id = 'langpack-{}@firefox.mozilla.org'.format(locale)
+    version = get_version(context)
+    url = get_api_url(context, UPLOAD_STATUS, id=langpack_id, version=version,
+                      upload_pk=upload_pk)
+    return await amo_get(context, url)
+
+
+async def get_signed_xpi(context, download_path, destination_path):
+    """Download the signed xpi from AMO.
+
+    Fetches from `download_path` (identified in a previous API call)
+    And stores at `destination_path`.
+    """
+    with open(destination_path, 'wb') as file:
+        await amo_download(context, download_path, file)
---
+++ addonscript/script.py
@@ -0,0 +1,70 @@
+import aiohttp
+import asyncio
+import os
+
+import scriptworker.client
+
+from addonscript.api import do_upload, get_upload_status, get_signed_xpi
+from addonscript.task import build_filelist
+from addonscript.xpi import get_langpack_info
+
+
+def _craft_aiohttp_connector(context):
+    return aiohttp.TCPConnector()
+
+
+def get_default_config(base_dir=None):
+    base_dir = base_dir or os.path.dirname(os.getcwd())
+    default_config = {
+        'work_dir': os.path.join(base_dir, 'work_dir'),
+        'schema_file': os.path.join(os.path.dirname(__file__), 'data', 'addonscript_task_schema.json'),
+    }
+    return default_config
+
+
+async def sign_addon(context, locale):
+    # Retry Me
+    if 1:
+        upload_data = await do_upload(context, locale)
+        pk = upload_data['pk']
+    # Retry me, instead of sleeping
+    if 1:
+        await asyncio.sleep(60*6)  # wait 6 minutes
+        upload_status = await get_upload_status(context, locale, pk)
+        if len(upload_status['files']) != 1:
+            raise Exception("Expected 1 file")
+        if (upload_status.get('validation_results') and
+                upload_status['validation_results']['errors']):
+            raise Exception("Automated Validation produced errors")
+        if not upload_status['files'][0]['signed']:
+            raise Exception("expected signed xpi")
+    # Retry me
+    if 1:
+        destination = 'foo'  # XXX: Fixme
+        await get_signed_xpi(context, upload_status['files'][0]['download_url'],
+                             destination)
+
+
+def build_locales_context(context):
+    langpack_info = []
+    for f in build_filelist(context):
+        current_info = get_langpack_info(context, f)
+        langpack_info.append(current_info)
+    context.locales = {locale_info['locale']: {
+                            'unsigned': locale_info['unsigned'],
+                            'version': locale_info['version'],
+                            'id': locale_info['id'],
+                        }
+                       for locale_info in langpack_info}
+
+
+async def async_main(context):
+    connector = _craft_aiohttp_connector(context)
+    async with aiohttp.ClientSession(connector=connector) as session:
+        context.session = session
+        work_dir = context.config['work_dir']
+        assert work_dir
+
+
+__name__ == '__main__' and \
+          scriptworker.client.sync_main(async_main, default_config=get_default_config())
