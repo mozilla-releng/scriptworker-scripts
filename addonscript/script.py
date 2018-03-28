3f20a237c0a432d6c69f1ee73f45512478a2a41c
Justin Wood <Callek@gmail.com>
2018-03-27 21:58:32 -0400

    Add better schema and prepare config for artifact_dir

--- addonscript/script.py
+++ addonscript/script.py
@@ -17,6 +17,7 @@ def get_default_config(base_dir=None):
     base_dir = base_dir or os.path.dirname(os.getcwd())
     default_config = {
         'work_dir': os.path.join(base_dir, 'work_dir'),
+        'artifact_dir': os.path.join(base_dir, 'artifact_dir'),
         'schema_file': os.path.join(os.path.dirname(__file__), 'data', 'addonscript_task_schema.json'),
     }
     return default_config
