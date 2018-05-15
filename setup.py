f610dda2eda08fb3f01d52bd82a085577ff52d0b
Aki Sasaki <aki@escapewindow.com>
2018-05-15 12:18:06 -0700

    py36, py37

--- setup.py
+++ setup.py
@@ -30,6 +30,6 @@ setup(
     license='MPL2',
     install_requires=requirements,
     classifiers=(
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ),
 )
