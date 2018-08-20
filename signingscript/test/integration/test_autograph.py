eb682d4031f9a9a85c925baa03403bb3c85f8ebe
Johan Lorenzo <jlorenzo@mozilla.com>
2018-08-20 18:42:42 +0200

    Don't use autograph dev instance anymore

--- signingscript/test/integration/test_autograph.py
+++ signingscript/test/integration/test_autograph.py
@@ -10,11 +10,10 @@ import tempfile
 import zipfile
 
 from mardor.cli import do_verify
-from scriptworker.utils import makedirs, download_file
+from scriptworker.utils import makedirs
 
 from signingscript.script import async_main
 from signingscript.test import context
-from signingscript.test.integration import skip_when_no_network
 
 
 assert context  # silence flake8
@@ -27,16 +26,16 @@ TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), '..', 'data')
 DEFAULT_SERVER_CONFIG = {
     'project:releng:signing:cert:dep-signing': [
         [
-            'https://autograph-hsm.dev.mozaws.net',
+            'http://localhost:5500',
             'alice',
-            'fs5wgcer9qj819kfptdlp8gm227ewxnzvsuj9ztycsx08hfhzu',
+            'abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmn',
             ['autograph_mar384'],
             'autograph'
         ],
         [
-            'https://autograph-hsm.dev.mozaws.net',
+            'http://localhost:5500',
             'bob',
-            '9vh6bhlc10y63ow2k4zke7k0c3l9hpr8mo96p92jmbfqngs9e7d',
+            '1234567890abcdefghijklmnopqrstuvwxyz1234567890abcd',
             ['autograph_apk'],
             'autograph'
         ],
@@ -97,29 +96,11 @@ DEFAULT_TASK = {
 }
 
 
-async def _download_file(url, abs_filename, chunk_size=128):
-    parent_dir = os.path.dirname(abs_filename)
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as resp:
-            makedirs(parent_dir)
-            with open(abs_filename, "wb") as fd:
-                while True:
-                    chunk = await resp.content.read(chunk_size)
-                    if not chunk:
-                        break
-                    fd.write(chunk)
-
-
-async def _download_files(urls, work_dir):
-    urls_per_on_disk_path = {
-        os.path.join(work_dir, 'cot/upstream-task-id1/', os.path.basename(url)): url
-        for url in urls
-    }
-    download_tasks = [_download_file(url, on_disk_path) for on_disk_path, url in urls_per_on_disk_path.items()]
-    print('Downloading original files...')
-    await asyncio.gather(*download_tasks)
-    print('Downloaded original files: {}'.format(urls))
-    return urls_per_on_disk_path.keys()
+def _copy_files_to_work_dir(file_name, context):
+    original_file_path = os.path.join(TEST_DATA_DIR, file_name)
+    copied_file_folder = os.path.join(context.config['work_dir'], 'cot', 'upstream-task-id1')
+    makedirs(copied_file_folder)
+    shutil.copy(original_file_path, copied_file_folder)
 
 
 def _write_server_config(tmpdir):
@@ -140,40 +121,20 @@ def _craft_task(file_names, signing_format):
 
 
 @pytest.mark.asyncio
-@skip_when_no_network
 async def test_integration_autograph_mar(context, tmpdir):
-    urls_to_download = (
-        'https://archive.mozilla.org/pub/firefox/nightly/2017/10/2017-10-02-22-02-04-mozilla-central/firefox-mozilla-central-58.0a1-linux-x86_64-en-US-20171001220301-20171002220204.partial.mar',
-        'https://archive.mozilla.org/pub/firefox/nightly/2017/10/2017-10-02-10-01-34-mozilla-central/firefox-mozilla-central-58.0a1-linux-x86_64-en-US-20171001220301-20171002100134.partial.mar',
-    )
-    file_names = [os.path.basename(url) for url in urls_to_download]
-
-    await _download_files(urls_to_download, context.config['work_dir'])
+    file_names = ['partial1.mar', 'partial2.mar']
+    for file_name in file_names:
+        _copy_files_to_work_dir(file_name, context)
 
     context.config['signing_server_config'] = _write_server_config(tmpdir)
     context.task = _craft_task(file_names, signing_format='autograph_mar384')
 
-    print('Running async_main...')
     await async_main(context)
-    print('async_main completed')
 
-    mar_pub_key_path = os.path.join(TEST_DATA_DIR, 'autograph_mar_dev_key.pub')
+    mar_pub_key_path = os.path.join(TEST_DATA_DIR, 'autograph_mar.pub')
     signed_paths = [os.path.join(context.config['artifact_dir'], file_name) for file_name in file_names]
     for signed_path in signed_paths:
-        assert do_verify(signed_path, keyfiles=[mar_pub_key_path]), "Signature doesn't match MAR_DEV_PUBLIC_KEY's"
-
-
-def _strip_apk_signature(files):
-    for file_ in files:
-        temp_zip_file_path = '{}.tmp'.format(file_)
-        with zipfile.ZipFile(file_, 'r') as original_zip:
-            with zipfile.ZipFile(temp_zip_file_path, 'w') as temp_zip_file:
-                for item in original_zip.infolist():
-                    buffer = original_zip.read(item.filename)
-                    if not item.filename.startswith('META-INF/'):
-                        temp_zip_file.writestr(item, buffer)
-
-            shutil.move(temp_zip_file_path, file_)
+        assert do_verify(signed_path, keyfiles=[mar_pub_key_path]), "Mar signature doesn't match expected key"
 
 
 def _instanciate_keystore(keystore_path, certificate_path, certificate_alias):
@@ -196,29 +157,22 @@ def _verify_apk_signature(keystore_path, apk_path, certificate_alias):
 
 
 @pytest.mark.asyncio
-@skip_when_no_network
 async def test_integration_autograph_apk(context, tmpdir):
-    urls_to_download = (
-        'https://queue.taskcluster.net/v1/task/UlL8a2zUTdqWjVFeBLIR0g/runs/0/artifacts/public/app-nightly-x86-release-signed-aligned.apk',
-        'https://queue.taskcluster.net/v1/task/UlL8a2zUTdqWjVFeBLIR0g/runs/0/artifacts/public/app-nightly-arm-release-signed-aligned.apk',
-    )
-    file_names = [os.path.basename(url) for url in urls_to_download]
-
-    downloaded_files = await _download_files(urls_to_download, context.config['work_dir'])
-    _strip_apk_signature(downloaded_files)
+    file_name = 'app.apk'
+    original_file_path = os.path.join(TEST_DATA_DIR, file_name)
+    copied_file_folder = os.path.join(context.config['work_dir'], 'cot', 'upstream-task-id1')
+    makedirs(copied_file_folder)
+    shutil.copy(original_file_path, copied_file_folder)
 
     context.config['signing_server_config'] = _write_server_config(tmpdir)
-    context.task = _craft_task(file_names, signing_format='autograph_apk')
+    context.task = _craft_task([file_name], signing_format='autograph_apk')
 
     keystore_path = os.path.join(tmpdir, 'keystore')
-    certificate_path = os.path.join(TEST_DATA_DIR, 'autograph_apk_dev_key.pub')
-    certificate_alias = 'autograph_apk_dev_key'
+    certificate_path = os.path.join(TEST_DATA_DIR, 'autograph_apk.pub')
+    certificate_alias = 'autograph_apk'
     _instanciate_keystore(keystore_path, certificate_path, certificate_alias)
 
-    print('Running async_main...')
     await async_main(context)
-    print('async_main completed')
 
-    signed_paths = [os.path.join(tmpdir, 'artifact', file_name) for file_name in file_names]
-    for signed_path in signed_paths:
-        assert _verify_apk_signature(keystore_path, signed_path, certificate_alias)
+    signed_path = os.path.join(tmpdir, 'artifact', file_name)
+    assert _verify_apk_signature(keystore_path, signed_path, certificate_alias)
