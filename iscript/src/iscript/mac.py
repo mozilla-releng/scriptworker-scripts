d76ae32716f6ea03ff3c18c922ae133324926826
Aki Sasaki <aki@escapewindow.com>
2019-05-09 13:48:12 -0700

    commenting out widevine until bug 1550548 is resolved

--- iscript/src/iscript/mac.py
+++ iscript/src/iscript/mac.py
@@ -462,12 +462,13 @@ async def sign_all_apps(config, key_config, entitlements_path, all_paths):
         futures.append(asyncio.ensure_future(verify_app_signature(app)))
     await raise_future_exceptions(futures)
     # sign widevine
-    futures = []
-    for app in all_paths:
-        futures.append(
-            asyncio.ensure_future(sign_widevine_dir(config, key_config, app.app_path))
-        )
-    await raise_future_exceptions(futures)
+    # XXX uncomment when we can contact autograph
+    # futures = []
+    # for app in all_paths:
+    #     futures.append(
+    #         asyncio.ensure_future(sign_widevine_dir(config, key_config, app.app_path))
+    #     )
+    # await raise_future_exceptions(futures)
 
 
 # get_bundle_id {{{1
