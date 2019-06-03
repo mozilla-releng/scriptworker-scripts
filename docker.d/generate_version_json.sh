ee25c42791f052e6ab744ca555595bb71f7c3ab5
Rail Aliiev <rail@mozilla.com>
2019-06-03 10:29:31 -0400

    Use proper build URL (#222)
    This fixes version.json's build URL and prints the digest at the end of
    the log

--- docker.d/generate_version_json.sh
+++ docker.d/generate_version_json.sh
@@ -9,6 +9,6 @@ cat > version.json <<EOF
     "commit": "${commit}",
     "version": "${version}",
     "source": "https://github.com/mozilla-releng/beetmoverscript",
-    "build": "https://github.com/mozilla-releng/beetmoverscript/commit/${commit}"
+    "build": "https://tools.taskcluster.net/tasks/${TASK_ID}"
 }
 EOF
