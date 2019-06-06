125092aabe1e3516cebdb4e2c21b0124bf85be19
Rail Aliiev <rail@mozilla.com>
2019-06-06 08:03:42 -0400

    Unify docker build and push (#223)
    In order to pass cloudops image verification we need to print the image
    digest in the log. The digest is printed as a part of `docker push` or
    `docker pull` (it's a part of registry specification).

--- docker.d/push_image.sh
+++ docker.d/push_image.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-test $IMAGE_TASK_ID
 test $SECRET_URL
 test $DOCKERHUB_EMAIL
 test $DOCKERHUB_USER
@@ -8,10 +7,5 @@ test $TAG
 
 dockerhub_password=$(curl $SECRET_URL | python -c 'import json, sys; a = json.load(sys.stdin); print a["secret"]["docker"]["password"]')
 
-WORKDIR=$(mktemp -d)
-cd $WORKDIR
-
-curl -L -o image.tar https://queue.taskcluster.net/v1/task/$IMAGE_TASK_ID/artifacts/public/image.tar
-docker load < image.tar
 docker login -e $DOCKERHUB_EMAIL -u $DOCKERHUB_USER -p $dockerhub_password
 docker push $TAG
