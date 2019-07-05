14402180965f9e127317c98ad539c7f253ed2d3f
Aki Sasaki <aki@escapewindow.com>
2019-07-04 20:02:27 -0700

    test_exceptions

--- treescript/tests/test_exceptions.py
+++ treescript/tests/test_exceptions.py
@@ -1,10 +1,17 @@
 import pytest
 
-from scriptworker.exceptions import ScriptWorkerTaskException
-from treescript.exceptions import TaskVerificationError, FailedSubprocess
+from scriptworker_client.exceptions import TaskError, TaskVerificationError
+import treescript.exceptions as exceptions
 
 
-@pytest.mark.parametrize("exc", (TaskVerificationError, FailedSubprocess))
-def test_exception(exc):
+@pytest.mark.parametrize(
+    "exc,from_",
+    (
+        (exceptions.TaskVerificationError, TaskVerificationError),
+        (exceptions.FailedSubprocess, TaskError),
+        (exceptions.TreeScriptError, TaskError),
+    ),
+)
+def test_exception(exc, from_):
     a = exc("x")
-    assert isinstance(a, ScriptWorkerTaskException)
+    assert isinstance(a, from_)
