5a36066036ed25a7dea31aee3b9b33b755808567
Rail Aliiev <rail@mozilla.com>
2019-07-15 19:20:06 -0400

    Switch to pip-compile-multi (#58)

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 import os
 from setuptools import setup, find_packages
 
+# We allow commented lines in this file
+project_dir = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(project_dir, 'requirements/base.in')) as f:
+    install_requires = [line.rstrip('\n') for line in f if not line.startswith('#')]
+
 
 def get_version():
     PATH = os.path.join(os.path.dirname(__file__), "balrogscript/_version.py")
@@ -30,19 +35,7 @@ setup(
         ],
     },
     license="MPL2",
-    install_requires=[
-        "arrow",
-        "balrogclient",
-        "cryptography",
-        "enum34",
-        "idna",
-        "ipaddress",
-        "jsonschema",
-        "mar",
-        "pyasn1",
-        "redo",
-        "six",
-    ],
+    install_requires=install_requires,
     classifiers=(
         'Intended Audience :: Developers',
         'Natural Language :: English',
