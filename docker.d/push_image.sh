d22147629ed24016292077824d3cd80928794e85
Rok Garbas <rok@garbas.si>
2019-07-18 16:37:46 +0200

    fix docker push (#50)

--- docker.d/push_image.sh
+++ docker.d/push_image.sh
@@ -3,11 +3,15 @@ set -e
 test $SECRET_URL
 test $DOCKERHUB_EMAIL
 test $DOCKERHUB_USER
+test $DOCKER_REPO
 test $DOCKER_TAG
 
-dockerhub_password=$(curl $SECRET_URL | python -c 'import json, sys; a = json.load(sys.stdin); print a["secret"]["docker"]["password"]')
 
-docker login -e $DOCKERHUB_EMAIL -u $DOCKERHUB_USER -p $dockerhub_password
+echo "=== Logging to docker hub ==="
+dockerhub_password=$(wget -qO- $SECRET_URL | jq '.["secret"]["docker"]["password"]')
+docker login --email=$DOCKERHUB_EMAIL --username=$DOCKERHUB_USER --password=$dockerhub_password
+
+echo "=== Pushing to docker hub ==="
 docker push $DOCKER_REPO:$DOCKER_TAG
 if [ "$DOCKER_RELEASE_TAG" = "" ]; then
   docker tag $DOCKER_REPO:$DOCKER_TAG $DOCKER_REPO:$DOCKER_RELEASE_TAG
