dbce8f44519c2e8034c13fbaa5979a64cfbf73fe
Aki Sasaki <aki@escapewindow.com>
2019-08-02 12:03:32 -0700

    point scriptworker_client/setup.py at requirements/base.in

--- scriptworker_client/setup.py
+++ scriptworker_client/setup.py
@@ -5,6 +5,11 @@ from setuptools import setup, find_packages
 with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "version.txt")) as f:
     version = f.read().rstrip()
 
+with open(
+    os.path.join(os.path.abspath(os.path.dirname(__file__)), "requirements", "base.in")
+) as f:
+    install_requires = f.read().splitlines()
+
 setup(
     name="scriptworker_client",
     version=version,
@@ -20,5 +25,5 @@ setup(
     include_package_data=True,
     zip_safe=True,
     license="MPL2",
-    install_requires=["aiohttp", "jsonschema", "PyYAML"],
+    install_requires=install_requires,
 )
