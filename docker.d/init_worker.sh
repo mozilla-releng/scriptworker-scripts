ef457b28de39ae667442f9c424ee3843e23fa452
Rail Aliiev <rail@mozilla.com>
2019-08-22 17:19:36 -0400

    Add Nazgul credentials

--- docker.d/init_worker.sh
+++ docker.d/init_worker.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 set -e
 
-# 
+#
 # Check that all required variables exist
 #
 test $COT_PRODUCT
 test $PROJECT_NAME
 test $BOUNCER_USERNAME
 test $BOUNCER_PASSWORD
+test $BOUNCER_USERNAME_NAZGUL
+test $BOUNCER_PASSWORD_NAZGUL
 
 case $COT_PRODUCT in
   firefox)
