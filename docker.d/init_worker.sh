0793a39f7d6998d5de95080cc0297998240ddb53
Rail Aliiev <rail@mozilla.com>
2019-08-23 11:46:37 -0400

    Typo fix (#33)

--- docker.d/init_worker.sh
+++ docker.d/init_worker.sh
@@ -8,9 +8,9 @@ case $ENV in
     test $MACAROON_BETA
     test $MACAROON_CANDIDATE
     test $MACAROON_ESR
-    export MACAROON_BETA_PATH=$CONFIGDIR/beta_macaroon.cfg
-    export MACAROON_CANDIDATE_PATH=$CONFIGDIR/candidate_macaroon.cfg
-    export MACAROON_ESR_PATH=$CONFIGDIR/esr_macaroon.cfg
+    export MACAROON_BETA_PATH=$CONFIG_DIR/beta_macaroon.cfg
+    export MACAROON_CANDIDATE_PATH=$CONFIG_DIR/candidate_macaroon.cfg
+    export MACAROON_ESR_PATH=$CONFIG_DIR/esr_macaroon.cfg
     echo $MACAROON_BETA | base64 -d > $MACAROON_BETA_PATH
     echo $MACAROON_CANDIDATE | base64 -d > $MACAROON_CANDIDATE_PATH
     echo $MACAROON_ESR | base64 -d > $MACAROON_ESR_PATH
