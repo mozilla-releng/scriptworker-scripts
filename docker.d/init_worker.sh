256dbc877f770d403909e2b18422d71e7dc22f2d
Rail Aliiev <rail@mozilla.com>
2019-08-28 16:51:31 -0400

    Fix shell tests (#249)

--- docker.d/init_worker.sh
+++ docker.d/init_worker.sh
@@ -5,26 +5,26 @@ case $COT_PRODUCT in
   firefox)
     case $ENV in
       dev|fake-prod)
-        test DEP_ID
-        test DEP_KEY
-        test DEP_PARTNER_ID
-        test DEP_PARTNER_KEY
-        test MAVEN_ID
-        test MAVEN_KEY
+        test $DEP_ID
+        test $DEP_KEY
+        test $DEP_PARTNER_ID
+        test $DEP_PARTNER_KEY
+        test $MAVEN_ID
+        test $MAVEN_KEY
         ;;
       prod)
-        test NIGHTLY_ID
-        test NIGHTLY_KEY
-        test RELEASE_ID
-        test RELEASE_KEY
-        test PARTNER_ID
-        test PARTNER_KEY
-        test DEP_ID
-        test DEP_KEY
-        test DEP_PARTNER_ID
-        test DEP_PARTNER_KEY
-        test MAVEN_ID
-        test MAVEN_KEY
+        test $NIGHTLY_ID
+        test $NIGHTLY_KEY
+        test $RELEASE_ID
+        test $RELEASE_KEY
+        test $PARTNER_ID
+        test $PARTNER_KEY
+        test $DEP_ID
+        test $DEP_KEY
+        test $DEP_PARTNER_ID
+        test $DEP_PARTNER_KEY
+        test $MAVEN_ID
+        test $MAVEN_KEY
         ;;
       *)
         exit 1
@@ -35,16 +35,16 @@ case $COT_PRODUCT in
   thunderbird)
     case $ENV in
       dev|fake-prod)
-        test DEP_ID
-        test DEP_KEY
+        test $DEP_ID
+        test $DEP_KEY
         ;;
       prod)
-        test NIGHTLY_ID
-        test NIGHTLY_KEY
-        test RELEASE_ID
-        test RELEASE_KEY
-        test DEP_ID
-        test DEP_KEY
+        test $NIGHTLY_ID
+        test $NIGHTLY_KEY
+        test $RELEASE_ID
+        test $RELEASE_KEY
+        test $DEP_ID
+        test $DEP_KEY
         ;;
       *)
         exit 1
@@ -53,15 +53,15 @@ case $COT_PRODUCT in
     export TASKCLUSTER_SCOPE_PREFIX="project:comm:thunderbird:releng:${PROJECT_NAME}:"
     ;;
   mobile)
-    test MAVEN_ID
-    test MAVEN_KEY
-    test MAVEN_SNAPSHOT_ID
-    test MAVEN_SNAPSHOT_KEY
+    test $MAVEN_ID
+    test $MAVEN_KEY
+    test $MAVEN_SNAPSHOT_ID
+    test $MAVEN_SNAPSHOT_KEY
     export TASKCLUSTER_SCOPE_PREFIX="project:mobile:android-components:releng:${PROJECT_NAME}:"
     ;;
   application-services)
-    test MAVEN_ID
-    test MAVEN_KEY
+    test $MAVEN_ID
+    test $MAVEN_KEY
     export TASKCLUSTER_SCOPE_PREFIX="project:mozilla:application-services:releng:${PROJECT_NAME}:"
     ;;
   *)
