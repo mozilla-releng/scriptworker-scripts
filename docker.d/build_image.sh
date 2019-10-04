0723392e6f69504f0dc5cd0e5109d182595348ab
Rok Garbas <rok@garbas.si>
2019-10-04 21:40:33 +0200

    another idea how to make it work

--- docker.d/build_image.sh
+++ docker.d/build_image.sh
@@ -4,8 +4,5 @@ set -e
 test $DOCKER_REPO
 test $DOCKER_TAG
 
-# we include general docker.d scripts with docker image
-cp ../docker.d/* docker.d/
-
 docker build -f Dockerfile -t $DOCKER_REPO:$DOCKER_TAG .
 docker save $DOCKER_REPO:$DOCKER_TAG > $1
