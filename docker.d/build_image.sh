98fb9ac455d159a6c6bd50e23d9a0ca9008919ca
Rok Garbas <rok@garbas.si>
2019-10-04 22:22:46 +0200

    another fix and removing debugging

--- docker.d/build_image.sh
+++ docker.d/build_image.sh
@@ -1,16 +1,8 @@
 #/bin/sh
 set -e
 
-echo "IS this working"
-echo $DOCKER_REPO
-echo $DOCKER_TAG
-
 test $DOCKER_REPO
 test $DOCKER_TAG
 
-
-echo "building docker image"
 docker build -f Dockerfile -t $DOCKER_REPO:$DOCKER_TAG .
-
-echo "saving docker image"
 docker save $DOCKER_REPO:$DOCKER_TAG > $1
