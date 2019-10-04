e4d7569faa14ae2768beea68e3da280e94811d68
Rok Garbas <rok@garbas.si>
2019-10-04 21:43:15 +0200

    add some debugging

--- docker.d/build_image.sh
+++ docker.d/build_image.sh
@@ -4,5 +4,11 @@ set -e
 test $DOCKER_REPO
 test $DOCKER_TAG
 
+echo $DOCKER_REPO
+echo $DOCKER_TAG
+
+echo "building docker image"
 docker build -f Dockerfile -t $DOCKER_REPO:$DOCKER_TAG .
+
+echo "saving docker image"
 docker save $DOCKER_REPO:$DOCKER_TAG > $1
