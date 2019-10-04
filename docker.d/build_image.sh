70876626a91b4918b128612930bb98559fd41522
Rok Garbas <rok@garbas.si>
2019-10-04 21:35:28 +0200

    copy general docker.d into docker build context

--- docker.d/build_image.sh
+++ docker.d/build_image.sh
@@ -4,5 +4,8 @@ set -e
 test $DOCKER_REPO
 test $DOCKER_TAG
 
+# we include general docker.d scripts with docker image
+cp ../docker.d/* docker.d/
+
 docker build -f Dockerfile -t $DOCKER_REPO:$DOCKER_TAG .
 docker save $DOCKER_REPO:$DOCKER_TAG > $1
