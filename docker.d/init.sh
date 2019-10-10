e92a60259b66b23efaef9d0a40efbebd342c07ee
Mihai Tabara <mtabara@mozilla.com>
2019-10-10 17:02:53 +0300

    Fix typo whitespace

--- docker.d/init.sh
+++ docker.d/init.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bas
 set -o errexit -o pipefail
 
 test_var_set() {
@@ -106,7 +106,7 @@ export WORKER_ID_PREFIX="${WORKER_TYPE}-"
 # ensure configuration folder exists we can write to it
 #
 mkdir -p -m 700 $CONFIG_DIR
- echo $ED25519_PRIVKEY > $ED25519_PRIVKEY_PATH
+echo $ED25519_PRIVKEY > $ED25519_PRIVKEY_PATH
 chmod 600 $ED25519_PRIVKEY_PATH
 
 #
