59d225fd173c7a589d9f17d2b06e3c4d95965bdb
Chris AtLee <catlee@mozilla.com>
2019-10-23 16:01:11 -0400

    Use async requests and temporary files for signing (#37)
    Also: drop py3.6, add 3.8 for signingscript

--- signingscript/setup.py
+++ signingscript/setup.py
@@ -4,10 +4,16 @@ from setuptools import setup, find_packages
 with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "version.txt")) as f:
     version = f.read().rstrip()
 
-with open(
-    os.path.join(os.path.abspath(os.path.dirname(__file__)), "requirements/base.in")
-) as f:
-    install_requires = f.readlines()
+install_requires = [
+    "arrow",
+    "mar",
+    "scriptworker",
+    "signtool",
+    "taskcluster",
+    "mohawk",
+    "winsign",
+    "macholib",
+]
 
 setup(
     name="signingscript",
