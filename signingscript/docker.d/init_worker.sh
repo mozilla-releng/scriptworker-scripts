43498017395fdbde8698a53ee868f3214748fa95
Aki Sasaki <aki@escapewindow.com>
2019-11-19 10:44:02 -0800

    fix /app hardcoded paths

--- signingscript/docker.d/init_worker.sh
+++ signingscript/docker.d/init_worker.sh
@@ -21,19 +21,19 @@ test_var_set 'PROJECT_NAME'
 test_var_set 'PUBLIC_IP'
 test_var_set 'TEMPLATE_DIR'
 
-export DMG_PATH=/app/files/dmg
-export HFSPLUS_PATH=/app/files/hfsplus
+export DMG_PATH=/app/signingscript/files/dmg
+export HFSPLUS_PATH=/app/signingscript/files/hfsplus
 export ZIPALIGN_PATH=/usr/bin/zipalign
 
 export PASSWORDS_PATH=$CONFIG_DIR/passwords.json
 export GPG_PUBKEY_PATH=$CONFIG_DIR/gpg_pubkey
 export WIDEVINE_CERT_PATH=$CONFIG_DIR/widevine.crt
 export AUTHENTICODE_TIMESTAMP_STYLE=null
-export AUTHENTICODE_CERT_PATH=/app/scriptworker/src/signingscript/data/authenticode_dep.crt
-export AUTHENTICODE_CROSS_CERT_PATH=/app/scriptworker/src/signingscript/data/authenticode_stub.crt
+export AUTHENTICODE_CERT_PATH=/app/signingscript/src/signingscript/data/authenticode_dep.crt
+export AUTHENTICODE_CROSS_CERT_PATH=/app/signingscript/src/signingscript/data/authenticode_stub.crt
 if [ "$ENV" == "prod" ]; then
   export AUTHENTICODE_TIMESTAMP_STYLE=old
-  export AUTHENTICODE_CERT_PATH=/app/scriptworker/src/signingscript/data/authenticode_prod.crt
+  export AUTHENTICODE_CERT_PATH=/app/signingscript/src/signingscript/data/authenticode_prod.crt
 fi
 
 echo $GPG_PUBKEY | base64 -d > $GPG_PUBKEY_PATH
