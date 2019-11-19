77372664d4b9cfb1034ae1249ab9c8abdaf92cbf
Aki Sasaki <aki@escapewindow.com>
2019-11-19 08:30:30 -0800

    allow for pinning individual scripts; set -e in pin-helper.sh

--- maintenance/pin-helper.sh
+++ maintenance/pin-helper.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 # This runs in docker to pin our requirements files.
+set -e
 SUFFIX=${SUFFIX:-txt}
+if [ $# -gt 0 ]; then
+    DIRS="$@"
+else
+    echo "Usage: pin-helper.sh [script(s)]"
+    exit 1
+fi
 
 pip install --upgrade pip
 pip install pip-compile-multi
@@ -8,19 +15,7 @@ pip install pip-compile-multi
 apt-get update
 apt-get install -y libsodium-dev
 
-for dir in \
-    addonscript \
-    balrogscript \
-    beetmoverscript \
-    bouncerscript \
-    iscript \
-    pushapkscript \
-    pushsnapscript \
-    scriptworker_client \
-    shipitscript \
-    signingscript \
-    treescript \
-    ; do
+for dir in $DIRS; do
     pushd $dir
     pip-compile-multi -g base -g test -o "$SUFFIX"
     chmod 644 requirements/*.txt
