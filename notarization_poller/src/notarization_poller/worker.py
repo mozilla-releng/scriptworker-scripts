d3eb5813b32c7fc86a317c88fb6b707948309187
Aki Sasaki <aki@escapewindow.com>
2019-11-25 19:47:30 -0800

    [notarization_poller] worker.py

---
+++ notarization_poller/src/notarization_poller/worker.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+"""Notarization poller worker functions.
+
+Attributes:
+    log (logging.Logger): the log object for the module.
+
+"""
+import asyncio
+import logging
+import signal
+import socket
+import sys
+import typing
+from asyncio import sleep
+
+import aiohttp
+import arrow
+from taskcluster.aio import Queue
+
+from notarization_poller.config import get_config_from_cmdln, update_logging_config
+from notarization_poller.task import Task, claim_work
+from scriptworker_client.constants import STATUSES
+from scriptworker_client.utils import makedirs, rm
+
+log = logging.getLogger(__name__)
+
+
+# RunTasks {{{1
+class RunTasks:
+    """Manages processing of Taskcluster tasks."""
+
+    def __init__(self, config):
+        """Initialize RunTasks."""
+        self.config = config
+        self.running_tasks = []
+        self.last_claim_work = arrow.get(0)
+        self.is_cancelled = False
+        self.future = None
+
+    async def invoke(self):
+        """Claims and processes Taskcluster work."""
+        while not self.is_cancelled:
+            num_tasks_to_claim = self.config["max_concurrent_tasks"] - len(self.running_tasks)
+            if num_tasks_to_claim > 0 and arrow.utcnow().timestamp - self.last_claim_work.timestamp >= self.config["claim_work_interval"]:
+                async with aiohttp.ClientSession() as session:
+                    queue = Queue(
+                        options={
+                            "credentials": {"accessToken": self.config["taskcluster_access_token"], "clientId": self.config["taskcluster_client_id"]},
+                            "rootUrl": self.config["taskcluster_root_url"],
+                        },
+                        session=session,
+                    )
+                    new_tasks = await self._run_cancellable(claim_work(self.config, queue, num_tasks=num_tasks_to_claim))
+                self.last_claim_work = arrow.utcnow()
+                for claim_task in new_tasks.get("tasks", []):
+                    new_task = Task(self.config, claim_task)
+                    self.running_tasks.append(new_task)
+            await self.prune_running_tasks()
+            sleep_time = self.last_claim_work.timestamp + self.config["claim_work_interval"] - arrow.utcnow().timestamp
+            sleep_time > 0 and await self._run_cancellable(sleep(sleep_time))
+
+    async def prune_running_tasks(self):
+        """Prune any complete tasks from ``self.running_tasks``."""
+        for task in self.running_tasks:
+            if task.complete:
+                self.running_tasks.remove(task)
+
+    async def _run_cancellable(self, coroutine: typing.Awaitable):
+        if not self.is_cancelled:
+            self.future = asyncio.ensure_future(coroutine)
+            result = await self.future
+            self.future = None
+            return result
+
+    async def cancel(self, status=STATUSES["worker-shutdown"]):
+        """Cancel any running tasks."""
+        self.is_cancelled = True
+        self.future and self.future.cancel()
+        try:
+            await asyncio.wait([asyncio.ensure_future(task.stop(status=status)) for task in self.running_tasks])
+        except (asyncio.CancelledError, ValueError):
+            pass
+
+
+# main {{{1
+def main(event_loop=None):
+    """Notarization poller entry point: get everything set up, then enter the main loop.
+
+    Args:
+        event_loop (asyncio.BaseEventLoop, optional): the event loop to use.
+            If None, use ``asyncio.get_event_loop()``. Defaults to None.
+
+    """
+    event_loop = event_loop or asyncio.get_event_loop()
+    done = False
+    config = get_config_from_cmdln(sys.argv[1:])
+    update_logging_config(config)
+
+    log.info("Notarization poller starting up at {} UTC".format(arrow.utcnow().format()))
+    log.info("Worker FQDN: {}".format(socket.getfqdn()))
+    rm(config["work_dir"])
+    makedirs(config["work_dir"])
+    running_tasks = RunTasks(config)
+
+    async def _handle_sigterm():
+        log.info("SIGTERM received; shutting down")
+        nonlocal done
+        done = True
+        await running_tasks.cancel()
+
+    async def _handle_sigusr1():
+        """Stop accepting new tasks."""
+        log.info("SIGUSR1 received; no more tasks will be taken")
+        nonlocal done
+        done = True
+
+    event_loop.add_signal_handler(signal.SIGTERM, lambda: asyncio.ensure_future(_handle_sigterm()))
+    event_loop.add_signal_handler(signal.SIGUSR1, lambda: asyncio.ensure_future(_handle_sigusr1()))
+
+    while not done:
+        try:
+            event_loop.run_until_complete(running_tasks.invoke())
+        except Exception:
+            log.critical("Fatal exception", exc_info=1)
+            raise
+    else:
+        log.info("Notarization poller stopped at {} UTC".format(arrow.utcnow().format()))
+        log.info("Worker FQDN: {}".format(socket.getfqdn()))
---
+++ notarization_poller/tests/test_worker.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python
+# coding=utf-8
+"""Test notarization_poller.worker
+"""
+import asyncio
+import json
+import os
+import signal
+import sys
+from copy import deepcopy
+
+import arrow
+import pytest
+
+import notarization_poller.worker as worker
+from notarization_poller.exceptions import WorkerError
+from notarization_poller.worker import RunTasks
+from scriptworker_client.constants import STATUSES
+
+
+# main {{{1
+def test_main(mocker, config, event_loop):
+    async def foo(*args):
+        raise WorkerError("foo")
+
+    fake_run_tasks = mocker.MagicMock()
+    fake_run_tasks.invoke = foo
+    mocker.patch.object(worker, "RunTasks", return_value=fake_run_tasks)
+
+    tmp = os.path.join(config["work_dir"], "foo")
+    with open(tmp, "w") as fh:
+        json.dump(config, fh)
+    mocker.patch.object(sys, "argv", new=["x", tmp])
+    with pytest.raises(WorkerError):
+        worker.main(event_loop=event_loop)
+
+
+@pytest.mark.parametrize("running", (True, False))
+def test_main_running_sigterm(mocker, config, event_loop, running):
+    """Test that sending SIGTERM causes the main loop to stop after the next
+    call to invoke."""
+    run_tasks_cancelled = event_loop.create_future()
+
+    class MockRunTasks:
+        async def cancel(*args):
+            run_tasks_cancelled.set_result(True)
+
+        async def invoke(*args):
+            os.kill(os.getpid(), signal.SIGTERM)
+
+    mrt = MockRunTasks()
+
+    mocker.patch.object(worker, "RunTasks", return_value=mrt)
+
+    tmp = os.path.join(config["work_dir"], "foo")
+    with open(tmp, "w") as fh:
+        json.dump(config, fh)
+    mocker.patch.object(sys, "argv", new=["x", tmp])
+    worker.main(event_loop=event_loop)
+
+    if running:
+        event_loop.run_until_complete(run_tasks_cancelled)
+        assert run_tasks_cancelled.result()
+
+
+@pytest.mark.parametrize("running", (True, False))
+def test_main_running_sigusr1(mocker, config, event_loop, running):
+    """Test that sending SIGUSR1 causes the main loop to stop after the next
+    call to invoke without cancelling the task."""
+    run_tasks_cancelled = event_loop.create_future()
+
+    class MockRunTasks:
+        async def cancel(*args):
+            run_tasks_cancelled.set_result(True)
+
+        async def invoke(*args):
+            os.kill(os.getpid(), signal.SIGUSR1)
+
+    mrt = MockRunTasks()
+
+    tmp = os.path.join(config["work_dir"], "foo")
+    with open(tmp, "w") as fh:
+        json.dump(config, fh)
+    mocker.patch.object(worker, "RunTasks", return_value=mrt)
+    mocker.patch.object(sys, "argv", new=["x", tmp])
+    worker.main(event_loop=event_loop)
+
+    assert not run_tasks_cancelled.done()
+
+
+# invoke {{{1
+@pytest.mark.asyncio
+async def test_mocker_invoke(config, mocker):
+    task = {"foo": "bar", "credentials": {"a": "b"}, "task": {"task_defn": True}}
+    stop_calls = []
+
+    rt = worker.RunTasks(config)
+
+    async def claim_work(*args, **kwargs):
+        return {"tasks": [deepcopy(task)]}
+
+    async def fake_sleep(*args, **kwargs):
+        await rt.cancel()
+
+    async def fake_stop(status=None):
+        stop_calls.append(status)
+
+    fake_task = mocker.MagicMock()
+    fake_task.complete = False
+    fake_task.stop = fake_stop
+
+    mocker.patch.object(worker, "claim_work", new=claim_work)
+    mocker.patch.object(worker, "Task", return_value=fake_task)
+    mocker.patch.object(worker, "Queue")
+    mocker.patch.object(worker, "sleep", new=fake_sleep)
+    await rt.invoke()
+    assert rt.is_cancelled
+    assert stop_calls == [STATUSES["worker-shutdown"]]
+    assert len(rt.running_tasks) == 1
+
+
+@pytest.mark.asyncio
+async def test_mocker_invoke_noop(config, mocker):
+    config["max_concurrent_tasks"] = 0
+    config["claim_work_interval"] = 30
+    rt = RunTasks(config)
+    rt.running_tasks = []
+    # This is needed, or we'll never sleep, and cancel_rt will never
+    # get a chance to run
+    rt.last_claim_work = arrow.utcnow()
+
+    async def cancel_rt():
+        await rt.cancel()
+
+    tasks = [asyncio.ensure_future(rt.invoke()), asyncio.ensure_future(cancel_rt())]
+    await asyncio.wait(tasks)
+    assert rt.is_cancelled
+    assert len(rt.running_tasks) == 0
+
+
+# prune_running_tasks {{{1
+@pytest.mark.asyncio
+async def test_prune_running_tasks(config, mocker):
+    task1 = mocker.MagicMock()
+    task1.complete = True
+    task2 = mocker.MagicMock()
+    task2.complete = False
+    task3 = mocker.MagicMock()
+    task3.complete = False
+    task4 = mocker.MagicMock()
+    task4.complete = True
+    rt = RunTasks(config)
+    rt.running_tasks = [task1, task2, task3, task4]
+    await rt.prune_running_tasks()
+    assert rt.running_tasks == [task2, task3]
+
+
+# run_cancellable {{{1
+@pytest.mark.asyncio
+async def test_run_cancellable(config):
+    async def return_true():
+        return True
+
+    rt = RunTasks(config)
+    future1 = return_true()
+    result = await rt._run_cancellable(future1)
+    assert result is True
+
+    # noop if is_cancelled
+    rt.is_cancelled = True
+    future2 = return_true()
+    result = await rt._run_cancellable(future2)
+    assert result is None
+    await future2  # silence warnings
