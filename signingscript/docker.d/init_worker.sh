919957fa23b53e1c37b50dffb9349a56b413d3d2
Aki Sasaki <aki@escapewindow.com>
2019-11-25 17:38:59 -0800

    add authenticode_ev to mpd001

--- signingscript/docker.d/init_worker.sh
+++ signingscript/docker.d/init_worker.sh
@@ -168,6 +168,8 @@ case $ENV in
       mpd001)
         test_var_set 'AUTOGRAPH_AUTHENTICODE_PASSWORD'
         test_var_set 'AUTOGRAPH_AUTHENTICODE_USERNAME'
+        test_var_set 'AUTOGRAPH_AUTHENTICODE_EV_PASSWORD'
+        test_var_set 'AUTOGRAPH_AUTHENTICODE_EV_USERNAME'
         test_var_set 'AUTHENTICODE_CERT_PATH'
         test_var_set 'AUTHENTICODE_CROSS_CERT_PATH'
         test_var_set 'AUTHENTICODE_TIMESTAMP_STYLE'
