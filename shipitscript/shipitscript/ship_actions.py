cc32287325d3a37f49bbabf6f18707a9774fbd4b
Mihai Tabara <mtabara@mozilla.com>
2019-11-29 16:18:48 +0000

    Shipitscript - bail if buildo is not 1

--- shipitscript/shipitscript/ship_actions.py
+++ shipitscript/shipitscript/ship_actions.py
@@ -1,4 +1,5 @@
 import logging
+import sys
 
 from shipitscript import pushlog_scan
 from shipitscript.shipitapi import Release_V2
@@ -91,6 +92,9 @@ def start_new_release(shipit_config, product, branch, version, revision, phase):
 
     # compute the build_number for the to-be-created release
     build_number = calculate_build_number(shipit_config, product, branch, version)
+    if build_number != 1:
+        log.info('Something is fishy in Ship-it. We may have a zombie build in Ship-it')
+        sys.exit(1)
 
     release_api, headers = get_shipit_api_instance(shipit_config)
     log.info('creating a new release...')
