32e4d8a0b7a00343069a69e5e58751e62465670a
Aki Sasaki <aki@escapewindow.com>
2019-12-04 17:18:43 -0800

    [notarization_poller] add exceptions.py

---
+++ notarization_poller/src/notarization_poller/exceptions.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+"""Notarization poller exceptions."""
+
+from scriptworker_client.constants import STATUSES
+
+
+class WorkerError(Exception):
+    """The base exception in scriptworker.
+
+    When raised inside of the run_loop loop, set the taskcluster task
+    status to at least ``self.exit_code``.
+
+    Attributes:
+        exit_code (int): this is set to 5 (internal-error).
+
+    """
+
+    exit_code = STATUSES["internal-error"]
+
+
+class RetryError(WorkerError):
+    """worker retry error.
+
+    Attributes:
+        exit_code (int): this is set to 4 (resource-unavailable)
+
+    """
+
+    exit_code = STATUSES["resource-unavailable"]
+
+
+class ConfigError(WorkerError):
+    """Invalid configuration provided to the worker.
+
+    Attributes:
+        exit_code (int): this is set to 5 (internal-error).
+
+    """
