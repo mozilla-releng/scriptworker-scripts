a802c3a1b0356c23cb2a789fe08eae29180ee731
Johan Lorenzo <jlorenzo@mozilla.com>
2020-01-06 11:28:22 +0100

    Bug 1606889 - pushsnapscript: Stop supporting tasks with no channel in payload

--- pushsnapscript/src/pushsnapscript/task.py
+++ pushsnapscript/src/pushsnapscript/task.py
@@ -1,5 +1,4 @@
 from scriptworker.exceptions import TaskVerificationError
-from scriptworker.utils import get_single_item_from_sequence
 
 SNAP_SCOPES_PREFIX = "project:releng:snapcraft:firefox:"
 
@@ -8,21 +7,14 @@ ALLOWED_CHANNELS = ("mock", *_CHANNELS_AUTHORIZED_TO_REACH_SNAP_STORE)
 
 
 def get_snap_channel(config, task):
-    if "channel" in task["payload"]:
-        channel = task["payload"]["channel"]
-        scope = SNAP_SCOPES_PREFIX + channel.split("/")[0]
-        if config["push_to_store"] and scope not in task["scopes"]:
-            raise TaskVerificationError(f"Channel {channel} not allowed, missing scope {scope}")
-    else:
-        scope = get_single_item_from_sequence(
-            task["scopes"],
-            lambda scope: scope.startswith(SNAP_SCOPES_PREFIX),
-            ErrorClass=TaskVerificationError,
-            no_item_error_message="No scope starts with {}".format(SNAP_SCOPES_PREFIX),
-            too_many_item_error_message="Too many scopes start with {}".format(SNAP_SCOPES_PREFIX),
-        )
-        channel = scope[len(SNAP_SCOPES_PREFIX) :]
-        channel = "esr/stable" if channel == "esr" else channel
+    payload = task["payload"]
+    if "channel" not in payload:
+        raise TaskVerificationError(f"channel must be defined in the task payload. Given payload: {payload}")
+
+    channel = payload["channel"]
+    scope = SNAP_SCOPES_PREFIX + channel.split("/")[0]
+    if config["push_to_store"] and scope not in task["scopes"]:
+        raise TaskVerificationError(f"Channel {channel} not allowed, missing scope {scope}")
 
     if channel not in ALLOWED_CHANNELS:
         raise TaskVerificationError('Channel "{}" is not allowed. Allowed ones are: {}'.format(channel, ALLOWED_CHANNELS))
