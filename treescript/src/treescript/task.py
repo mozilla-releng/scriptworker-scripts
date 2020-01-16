2bd46e62e113f8a182729f20b8090105b7a6fa67
Tom Prince <mozilla@hocat.ca>
2020-01-16 15:08:36 -0700

    [treescript] Update docstrings.

--- treescript/src/treescript/task.py
+++ treescript/src/treescript/task.py
@@ -186,17 +186,17 @@ def get_ignore_closed_tree(task):
 
 # task_action_types {{{1
 def task_action_types(config, task):
-    """Extract task actions as scope definitions.
+    """Extract task actions from task payload.
 
     Args:
         config (dict): the running config.
         task (dict): the task definition.
 
     Raises:
-        TaskVerificationError: if the number of cert scopes is not 1.
+        TaskVerificationError: if unknown actions are specified.
 
     Returns:
-        str: the cert type.
+        set: the set of specified actions
 
     """
     actions = set(task["payload"].get("actions", []))
@@ -210,9 +210,11 @@ def task_action_types(config, task):
 
 # is_dry_run {{{1
 def should_push(task, actions):
-    """Extract task force_dry_run feature.
+    """Determine whether this task should push the changes it makes.
 
-    This is meant as a means to do a dry-run even if the task has the push action scope.
+    If `dry_run` is true on the task, there will not be a push.
+    Otherwise, if `push` is specified, that determines if there should be a push.
+    Otherwise, there is a push if the `push` action is specified.
 
     Args:
         task (dict): the task definition.
@@ -221,7 +223,7 @@ def should_push(task, actions):
         TaskVerificationError: if the number of cert scopes is not 1.
 
     Returns:
-        str: the cert type.
+        bool: whether this task should push
 
     """
     dry_run = task["payload"].get("dry_run", False)
