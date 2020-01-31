792bef0e4be01db607d152945ac61f47ce5c434e
Simon Fraser <sfraser@mozilla.com>
2020-01-31 10:58:20 +0100

    Add test merge behaviour

--- treescript/src/treescript/merge_config.py
+++ treescript/src/treescript/merge_config.py
@@ -71,4 +71,37 @@ merge_configs = {
         "base_tag": "FIREFOX_ESR_{major_version}_BASE",
         "requires_head_merge": False,
     },
+    "central_to_maple": {
+        "version_files": ["browser/config/version.txt", "config/milestone.txt"],
+        "version_files_suffix": ["browser/config/version_display.txt"],
+        "version_suffix": "b1",
+        "copy_files": [],
+        "replacements": [
+            # File, from, to
+            (f, "ac_add_options --with-branding=browser/branding/nightly", "ac_add_options --enable-official-branding")
+            for f in [
+                "browser/config/mozconfigs/linux32/l10n-mozconfig",
+                "browser/config/mozconfigs/linux64/l10n-mozconfig",
+                "browser/config/mozconfigs/win32/l10n-mozconfig",
+                "browser/config/mozconfigs/win64/l10n-mozconfig",
+                "browser/config/mozconfigs/win64-aarch64/l10n-mozconfig",
+                "browser/config/mozconfigs/macosx64/l10n-mozconfig",
+            ]
+        ]
+        + [
+            # File, from, to
+            ("build/mozconfig.common", "MOZ_REQUIRE_SIGNING=${MOZ_REQUIRE_SIGNING-0}", "MOZ_REQUIRE_SIGNING=${MOZ_REQUIRE_SIGNING-1}"),
+            (
+                "build/mozconfig.common",
+                "# Disable enforcing that add-ons are signed by the trusted root",
+                "# Enable enforcing that add-ons are signed by the trusted root",
+            ),
+        ],
+        "from_branch": "central",
+        "to_branch": "beta",
+        "push_repositories": {"from": "https://hg.mozilla.org/mozilla-central", "to": "https://hg.mozilla.org/projects/maple"},
+        "merge_old_head": True,
+        "base_tag": "FIREFOX_BETA_{major_version}_BASE",
+        "end_tag": "FIREFOX_BETA_{major_version}_END",
+    },
 }
