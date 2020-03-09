f1fef709c1d1c200bc2266428b40542856f4511f
Simon Fraser <sfraser@mozilla.com>
2020-03-09 23:38:28 +0000

    Adjust when tags are preserved

--- treescript/src/treescript/merges.py
+++ treescript/src/treescript/merges.py
@@ -115,31 +115,30 @@ async def do_merge(config, task, repo_path):
             "Merge old head via |hg debugsetparents {} {}| CLOSED TREE DONTBUILD a=release".format(tagged_from_rev, to_branch),
             repo_path=repo_path,
         )
-
-    # Preserve tags.
-    patch_file = os.path.join(os.path.dirname(repo_path), "patch_file")
-    tag_diff = await run_hg_command(config, "diff", "-r", to_branch, os.path.join(repo_path, ".hgtags"), "-U9", return_output=True, repo_path=repo_path)
-    with open(patch_file, "w") as fh:
-        fh.write(tag_diff)
-    await run_command(["patch", "-R", "-p1", patch_file], cwd=repo_path)
-    os.unlink(patch_file)
-    with open(os.path.join(repo_path, ".hgtags"), "a") as fh:
-        # Skip four header lines
-        for line in tag_diff.splitlines()[4:]:
-            # We only care about additions.
-            if not line.startswith("+"):
-                continue
-            line = line.replace("+", "")
-            changeset, _ = line.split()
-            # Check for bogus changeset
-            if len(changeset) != 40:
-                continue
-            fh.write(f"{line}\n")
-    status_out = await run_hg_command(config, "status", os.path.join(repo_path, ".hgtags"), return_output=True, repo_path=repo_path)
-    if status_out:
-        await run_hg_command(config, "commit", "-m", "Preserve old tags after debusetparents. CLOSED TREE DONTBUILD a=release", repo_path=repo_path)
-    else:
-        log.info("No changes to .hgtags, not performing commit.")
+        # Preserve tags.
+        patch_file = os.path.join(os.path.dirname(repo_path), "patch_file")
+        tag_diff = await run_hg_command(config, "diff", "-r", to_branch, os.path.join(repo_path, ".hgtags"), "-U9", return_output=True, repo_path=repo_path)
+        with open(patch_file, "w") as fh:
+            fh.write(tag_diff)
+        await run_command(["patch", "-R", "-p1", patch_file], cwd=repo_path)
+        os.unlink(patch_file)
+        with open(os.path.join(repo_path, ".hgtags"), "a") as fh:
+            # Skip four header lines
+            for line in tag_diff.splitlines()[4:]:
+                # We only care about additions.
+                if not line.startswith("+"):
+                    continue
+                line = line.replace("+", "")
+                changeset, _ = line.split()
+                # Check for bogus changeset
+                if len(changeset) != 40:
+                    continue
+                fh.write(f"{line}\n")
+        status_out = await run_hg_command(config, "status", os.path.join(repo_path, ".hgtags"), return_output=True, repo_path=repo_path)
+        if status_out:
+            await run_hg_command(config, "commit", "-m", "Preserve old tags after debusetparents. CLOSED TREE DONTBUILD a=release", repo_path=repo_path)
+        else:
+            log.info("No changes to .hgtags, not performing commit.")
 
     end_tag = merge_config.get("end_tag")  # tag the end of the to repo
     if end_tag:
