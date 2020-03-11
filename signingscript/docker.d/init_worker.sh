437503ff72768d9a54caa3a8dce6748c09eecb49
Justin Wood <Callek@gmail.com>
2020-03-11 12:52:05 -0400

    Use dep cert as well for mpd

--- signingscript/docker.d/init_worker.sh
+++ signingscript/docker.d/init_worker.sh
@@ -53,6 +53,7 @@ case $COT_PRODUCT in
   xpi)
     ;;
   mpd001)
+    export AUTHENTICODE_CERT_PATH=/app/signingscript/src/signingscript/data/authenticode_mpd_dep.crt
     if [ "$ENV" == "prod" ]; then
       export AUTHENTICODE_CERT_PATH=/app/signingscript/src/signingscript/data/authenticode_mpd_prod.crt
     fi
