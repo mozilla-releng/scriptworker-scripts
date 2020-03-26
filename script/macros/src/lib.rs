0ab58b0a0d446a3629939ba7a31332f8df27629e
Tom Prince <mozilla@hocat.ca>
2020-03-26 01:43:45 -0600

    rust sketch

---
+++ script/macros/src/lib.rs
@@ -0,0 +1,25 @@
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+use quote::quote;
+
+#[proc_macro_attribute]
+#[cfg(not(test))] // Work around for rust-lang/rust#62127
+pub fn main(args: TokenStream, item: TokenStream) -> TokenStream {
+    let input = syn::parse_macro_input!(item as syn::ItemFn);
+    let args = syn::parse_macro_input!(args as syn::AttributeArgs);
+
+    if !args.is_empty() {
+        panic!("???")
+    }
+
+    let name = &input.sig.ident;
+
+    let result = quote! {
+        #input
+        fn main() {
+            ::scriptworker_script::scriptworker_main(#name)
+        }
+    };
+    result.into()
+}
---
+++ script/src/lib.rs
@@ -0,0 +1,76 @@
+use serde::de::DeserializeOwned;
+use std::path::{Path, PathBuf};
+
+use clap::{App, Arg};
+
+mod error;
+pub use error::Error;
+
+pub mod task;
+pub use task::Task;
+
+pub struct Context {
+    work_dir: PathBuf,
+}
+
+fn init_config<T>() -> Result<(T, PathBuf), Error>
+where
+    T: DeserializeOwned,
+{
+    let matches = App::new("scriptworker")
+        .arg(Arg::with_name("CONFIG_FILE").index(1).required(true))
+        .arg(Arg::with_name("WORK_DIR").index(2).required(true))
+        .get_matches();
+
+    let config_file = matches.value_of_os("CONFIG_FILE").unwrap();
+    let work_dir = Path::new(matches.value_of_os("WORK_DIR").unwrap());
+    Ok((
+        serde_yaml::from_reader(std::fs::File::open(config_file)?)?,
+        work_dir.into(),
+    ))
+}
+
+pub fn load_secrets<'de, D, T>(deserializer: D) -> Result<T, D::Error>
+where
+    D: serde::Deserializer<'de>,
+    T: DeserializeOwned,
+{
+    let secret_file_path: String = serde::Deserialize::deserialize(deserializer)?;
+    let secret_file = std::fs::File::open(secret_file_path)
+        .map_err(|_| serde::de::Error::custom("Could not open secret file."))?;
+    Ok(serde_yaml::from_reader(secret_file)
+        .map_err(|_| serde::de::Error::custom("Could not parse secrets file."))?)
+}
+
+pub fn scriptworker_main<Config, A, E>(
+    do_work: impl FnOnce(Config, &Context, Task<A, E>) -> Result<(), Error>,
+) where
+    Config: DeserializeOwned,
+    A: DeserializeOwned,
+    E: DeserializeOwned,
+{
+    let result = (|| {
+        let (config, work_dir) = init_config::<Config>()?;
+        // TODO: logging
+        let task_filename = work_dir.join("task.json");
+        let task = Task::<A, E>::load(&task_filename)?;
+
+        do_work(config, &Context { work_dir }, task)
+    })();
+    match result {
+        Ok(()) => std::process::exit(0),
+        Err(err) => {
+            if let Error::MalformedPayload(message) = &err {
+                std::println!("{}", &message)
+            }
+            if let Error::InternalError(message) = &err {
+                std::println!("{}", &message)
+            }
+            std::process::exit(err.exit_code())
+        }
+    }
+    // TODO: Statuses
+}
+
+#[cfg(not(test))] // Work around for rust-lang/rust#62127
+pub use scriptworker_script_macros::main;
