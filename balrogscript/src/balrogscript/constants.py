7828e91c89fdef5dd3091737fb84a8defb5755d0
Justin Wood <Callek@gmail.com>
2020-04-29 09:06:49 -0400

    Make sure new behaviors are in VALID_ACTIONS

--- balrogscript/src/balrogscript/constants.py
+++ balrogscript/src/balrogscript/constants.py
@@ -4,4 +4,4 @@ Attributes:
     VALID_ACTIONS (tuple): the available actions supported by balrogscript.
 
 """
-VALID_ACTIONS = ("submit-locale", "submit-toplevel", "schedule", "set-readonly")
+VALID_ACTIONS = ("submit-locale", "submit-toplevel", "schedule", "set-readonly", "v2-submit-locale", "v2-submit-toplevel")
