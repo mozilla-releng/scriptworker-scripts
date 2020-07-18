3125f402374c0304d10ad61e801e01c3b87f068b
Tom Prince <mozilla@hocat.ca>
2020-07-17 18:19:14 -0600

    Don't include scriptworker version in dockerflow metadata.
    Since we've moved to cloudops deployed workers, we don't ever
    update the version number, so including is not useful

--- docker.d/generate_version_json.sh
+++ docker.d/generate_version_json.sh
@@ -5,12 +5,10 @@ test $HEAD_REV
 test $TASK_ID
 test $TASKCLUSTER_ROOT_URL
 test $REPO_URL
-test $PROJECT_NAME
 
 cat > version.json <<EOF
 {
     "commit": "${GIT_HEAD_REV}",
-    "version": "$(cat ${PROJECT_NAME}/version.txt)",
     "source": "${REPO_URL}",
     "build": "${TASKCLUSTER_ROOT_URL}/tasks/${TASK_ID}"
 }
