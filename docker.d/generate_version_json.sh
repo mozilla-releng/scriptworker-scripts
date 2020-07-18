6e1c9dcf876a4a69fd9c3864654377011540a2a5
Tom Prince <mozilla@hocat.ca>
2020-07-17 18:20:40 -0600

    Use run-task compatible environment variables for building images.

--- docker.d/generate_version_json.sh
+++ docker.d/generate_version_json.sh
@@ -1,15 +1,15 @@
 #/bin/sh
 set -e
 
-test $HEAD_REV
+test $SCRIPTWORKER_HEAD_REV
 test $TASK_ID
 test $TASKCLUSTER_ROOT_URL
-test $REPO_URL
+test $SCRIPTWORKER_HEAD_REPOSITORY
 
 cat > version.json <<EOF
 {
-    "commit": "${GIT_HEAD_REV}",
-    "source": "${REPO_URL}",
+    "commit": "${SCRIPTWORKER_HEAD_REV}",
+    "source": "${SCRIPTWORKER_HEAD_REPOSITORY}",
     "build": "${TASKCLUSTER_ROOT_URL}/tasks/${TASK_ID}"
 }
 EOF
