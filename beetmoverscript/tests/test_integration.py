23bad71a1f572c65fc924ce35cfa21bd9e4ad7f4
Alex Lopez <39034209+alopezz@users.noreply.github.com>
2020-11-06 15:02:27 +0100

    Bug 1672691 - part 3: [beetmoverscript] Add integration tests to beetmover (push-to-nightly) (#290)
    * Add beetmoverscript integration test for firefox desktop nightly
    
    * Add the rest of the testcases for beetmoverscript push-to-nightly/candidates
    
    * Configure and create artifact dir within integration tests
    
    * Put all test cases in the same test function
    
    * Change how the config is set in beetmoverscript integration tests

--- beetmoverscript/tests/test_integration.py
+++ beetmoverscript/tests/test_integration.py
@@ -5,8 +5,9 @@ from unittest.mock import Mock
 import pytest
 
 import beetmoverscript.script
+from beetmoverscript.constants import BUILDHUB_ARTIFACT
 from beetmoverscript.script import main
-from beetmoverscript.utils import load_json
+from beetmoverscript.utils import load_json, write_json
 
 from . import get_fake_valid_config
 
@@ -15,29 +16,32 @@ def get_test_task(task_name):
     return load_json(f"tests/task_examples/{task_name}.json")
 
 
-_CONFIG_MAP = {
-    "android-components": {
-        "taskcluster_scope_prefix": "project:mobile:android-components:releng:beetmover:",
-        "bucket_config_key": "maven-nightly-staging",
-        "bucket_name": "nightly_components",
-    },
-    "app-services": {
-        "taskcluster_scope_prefix": "project:mozilla:app-services:releng:beetmover:",
-        "bucket_config_key": "maven-production",
-        "bucket_name": "appservices",
-    },
-    "geckoview": {"taskcluster_scope_prefix": "project:releng:beetmover:", "bucket_config_key": "maven-production", "bucket_name": "geckoview"},
-    "glean": {"taskcluster_scope_prefix": "project:mozilla:glean:releng:beetmover:", "bucket_config_key": "maven-production", "bucket_name": "telemetry"},
-}
-
-
-def get_config(config_name):
+def get_config(scope_prefix):
     config = get_fake_valid_config()
-    config_props = _CONFIG_MAP[config_name]
-    config["taskcluster_scope_prefix"] = config_props["taskcluster_scope_prefix"]
-    config["bucket_config"][config_props["bucket_config_key"]] = {
-        "buckets": {config_props["bucket_name"]: "dummy"},
-        "credentials": {"id": "dummy", "key": "dummy"},
+    credentials = {"id": "dummy", "key": "dummy"}
+    url_prefix = "https://url.prefix"
+    config["taskcluster_scope_prefix"] = scope_prefix
+    config["bucket_config"] = {
+        "maven-nightly-staging": {
+            "buckets": {"nightly_components": "dummy"},
+            "credentials": credentials,
+            "url_prefix": url_prefix,
+        },
+        "maven-production": {
+            "buckets": {"appservices": "dummy", "geckoview": "dummy", "telemetry": "dummy"},
+            "credentials": credentials,
+            "url_prefix": url_prefix,
+        },
+        "nightly": {
+            "buckets": {"firefox": "dummy"},
+            "credentials": credentials,
+            "url_prefix": url_prefix,
+        },
+        "release": {
+            "buckets": {"devedition": "dummy", "firefox": "dummy"},
+            "credentials": credentials,
+            "url_prefix": url_prefix,
+        },
     }
     return config
 
@@ -53,8 +57,12 @@ def create_dummy_artifacts_for_task(task, root_path):
 
 
 def create_dummy_artifact(file_path):
-    with open(file_path, "wb") as f:
-        f.write(b"some data")
+    if file_path.name == BUILDHUB_ARTIFACT:
+        with open(file_path, "w") as f:
+            write_json(file_path, {"download": {}})
+    else:
+        with open(file_path, "wb") as f:
+            f.write(b"some data")
 
 
 def get_paths_for_task(task):
@@ -119,6 +127,12 @@ def prepare_scriptworker_config(path, config, task):
     work_dir.mkdir()
     config["work_dir"] = str(work_dir)
 
+    # An artifact dir is needed with a public subdir for push-to-nightly paths
+    artifact_dir = path / "artifact"
+    artifact_dir.mkdir()
+    (artifact_dir / "public").mkdir()
+    config["artifact_dir"] = str(artifact_dir)
+
     config_path = path / "config.json"
     with open(config_path, "w") as config_file:
         json.dump(config, config_file)
@@ -132,23 +146,26 @@ def prepare_scriptworker_config(path, config, task):
 
 
 @pytest.mark.parametrize(
-    "config_name, task_name, expected_number_of_artifacts",
+    "task_name, scope_prefix, expected_number_of_moved_artifacts",
     (
-        ("android-components", "android_components_nightly", 12),
-        ("geckoview", "geckoview_nightly", 16),
-        ("app-services", "application_services_release", 12),
-        ("glean", "glean_release", 20),
+        ("android_components_nightly", "project:mobile:android-components:releng:beetmover:", 12),
+        ("application_services_release", "project:mozilla:app-services:releng:beetmover:", 12),
+        ("geckoview_nightly", "project:releng:beetmover:", 16),
+        ("glean_release", "project:mozilla:glean:releng:beetmover:", 20),
+        ("firefox_desktop_nightly", "project:releng:beetmover:", 52),
+        ("firefox_langpacks_beta", "project:releng:beetmover:", 1),
+        ("firefox_generated_screenshots_release", "project:releng:beetmover:", 7),
     ),
 )
-def test_main_push_to_maven(config_name, task_name, expected_number_of_artifacts, tmp_path, boto3_client_mock, aiohttp_session_mock):
-    config = get_config(config_name)
+def test_main_maven_nightly_candidates(task_name, scope_prefix, expected_number_of_moved_artifacts, tmp_path, boto3_client_mock, aiohttp_session_mock):
+    config = get_config(scope_prefix)
     task = get_test_task(task_name)
     scriptworker_config = prepare_scriptworker_config(tmp_path, config, task)
 
     main(config_path=scriptworker_config)
 
     # Number of artifacts put matches expected
-    assert len(aiohttp_session_mock["put"]) == expected_number_of_artifacts
+    assert len(aiohttp_session_mock["put"]) == expected_number_of_moved_artifacts
 
     # Check paths
     expected_paths = get_paths_for_task(task)
@@ -157,6 +174,7 @@ def test_main_push_to_maven(config_name, task_name, expected_number_of_artifacts
     assert expected_signed_paths == set(put_call["url"] for put_call in aiohttp_session_mock["put"])
     # Check that the source matches expected for every destination
     for put_call in aiohttp_session_mock["put"]:
-        assert put_call["data"] == b"some data"
+        if not put_call["url"].endswith(".json"):
+            assert put_call["data"] == b"some data"
         _, unsigned_url = put_call["url"].split("+")
         assert put_call["source"].endswith(expected_paths[unsigned_url])
