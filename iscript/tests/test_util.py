728735338146f51249e3856b325b971cf95fbbca
Aki Sasaki <aki@mozilla.com>
2020-12-02 08:58:44 -0800

    [RELENG-117] - iscript vpn (#302)
    * sign_config includes product config
    
    * fix tests
    
    * avoid flake8 `'sign_app' is too complex` error
    
    * use ditto for everything

--- iscript/tests/test_util.py
+++ iscript/tests/test_util.py
@@ -2,29 +2,37 @@
 # coding=utf-8
 """Test iscript.util
 """
+from copy import deepcopy
+
 import pytest
 
 import iscript.util as util
+from iscript.constants import PRODUCT_CONFIG
 from iscript.exceptions import IScriptError
 
 
-# get_key_config {{{1
+# get_sign_config {{{1
 @pytest.mark.parametrize(
-    "scopes, base_key, key, raises",
+    "scopes, base_key, key, product, raises",
     (
-        (("scope:prefix:cert:dep-signing",), "mac_config", "dep", False),
-        (("scope:prefix:cert:nightly-signing",), "mac_config", "nightly", False),
-        (("invalid_scope"), "mac_config", None, True),
-        (["scope:prefix:cert:nightly-signing", "scope:prefix:cert:dep-signing"], "mac_config", "", True),
-        (("scope:prefix:cert:dep-signing",), "invalid_base_key", "", True),
+        (("scope:prefix:cert:dep-signing",), "mac_config", "dep", "firefox", False),
+        (("scope:prefix:cert:nightly-signing",), "mac_config", "nightly", "firefox", False),
+        (("scope:prefix:cert:nightly-signing",), "mac_config", "nightly", "mozillavpn", False),
+        (("invalid_scope"), "mac_config", None, "firefox", True),
+        (["scope:prefix:cert:nightly-signing", "scope:prefix:cert:dep-signing"], "mac_config", "", "firefox", True),
+        (("scope:prefix:cert:dep-signing",), "invalid_base_key", "", "firefox", True),
+        (("scope:prefix:cert:dep-signing",), "mac_config", "dep", "bad_product", True),
     ),
 )
-def test_get_config_key(scopes, base_key, key, raises):
+def test_get_config_key(scopes, base_key, key, product, raises):
     """``get_config_key`` returns the correct subconfig."""
     config = {"taskcluster_scope_prefix": "scope:prefix:", "mac_config": {"dep": {"key": "dep"}, "nightly": {"key": "nightly"}}}
-    task = {"scopes": scopes}
+    task = {"scopes": scopes, "payload": {"product": product}}
     if raises:
         with pytest.raises(IScriptError):
-            util.get_key_config(config, task, base_key=base_key)
+            util.get_sign_config(config, task, base_key=base_key)
     else:
-        assert util.get_key_config(config, task, base_key=base_key) == config[base_key][key]
+        sign_config = util.get_sign_config(config, task, base_key=base_key)
+        expected = deepcopy(PRODUCT_CONFIG[base_key][product])
+        expected.update(config[base_key][key])
+        assert sign_config == expected
