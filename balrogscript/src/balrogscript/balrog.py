1b5e9eb39013f42ac3368af2482e93697ce969a0
Aki Sasaki <aki@mozilla.com>
2021-08-03 17:58:49 -0700

    some more wip

--- balrogscript/src/balrogscript/balrog.py
+++ balrogscript/src/balrogscript/balrog.py
@@ -10,20 +10,45 @@ from redo import retry
 log = logging.getLogger(__name__)
 
 
-def update_rules(rules, api_root, dummy, auth0_secrets):
-    for rule in rules:
-        # XXX get former data? or do we just update the fields mentioned?
-        data = deepcopy(rule)
-        rule_id = data.pop("rule_id")
-        # XXX scheduled_change support?
-        # XXX deletion support?
-        # XXX fallbackMapping support?
-        # XXX support creation of rules?
-        # XXX any verification here?
-        #     possibly download the existing rule
-        #     only support allowlisted products/channels?
+def update_existing_rule(rule_data, api_root, dummy, auth0_secrets):
+    """Update an existing rule.
+    """
+    # XXX should we pass in the Rule or ScheduledRuleChange object, for clean arch?
+    rule_id = rule_data.pop("rule_id")  # XXX better error than a KeyError?
+    # XXX scheduled_change support?
+    rule_obj = Rule(api_root=api_root, auth0_secrets=auth0_secrets, rule_id=rule_id)
+    existing_data, existing_data_version = Rule.get_data()  # XXX verify this works
+    log.info("Rule %s data version %s:\n%s", rule_id, existing_data_version, pprint.pformat(existing_data))
+    # XXX fallbackMapping support?
+    # XXX support creation of rules?
+    # XXX any verification here?
+    #     only support allowlisted products/channels?
+    log.info("Updating rule %s with data\n%s", rule_id, pprint.pformat(rule_data))
+    # XXX ScheduledRuleChange.add_scheduled_rule_change
+    retry(rule_obj.update_rule, kwargs=rule_data, sleeptime=2, max_sleeptime=2, attempts=10)
 
-        log.info(f"Updating rule {rule_id} with data\n{pprint.pformat(data)}")
-        rule_obj = Rule(api_root=api_root, auth0_secrets=auth0_secrets, rule_id=rule_id)
-        # ScheduledRuleChange?
-        retry(rule_obj.update_rule, kwargs=data, sleeptime=2, max_sleeptime=2, attempts=10)
+
+def create_rule(rule_data, api_root, dummy, auth0_secrets):
+    """Create a rule that doesn't exist.
+    """
+    raise NotImplementedError("create_rule not written yet")
+
+
+def delete_existing_rule(rule_data, api_root, dummy, auth0_secrets):
+    """
+    """
+    raise NotImplementedError("delete_existing_rule not written yet")
+
+
+def update_rules(rules, api_root, dummy, auth0_secrets):
+    """
+    """
+    # XXX rules api is only api v1??
+    for rule in rules:
+        rule_data = deepcopy(rule)
+        if rule_data.pop("existingRule", True):
+            # XXX deletion support? Check for bool, write+call delete_existing_rule
+            update_existing_rule(rule_data, api_root, dummy, auth0_secrets)
+        else:
+            create_rule(rule_data, api_root, dummy, auth0_secrets)
+
