0ad51b4c7a4663f1ff2d0f19bf7b762821cb84a5
Aki Sasaki <aki@mozilla.com>
2021-08-26 17:43:48 -0600

    delete_existing_rule

--- balrogscript/src/balrogscript/balrog.py
+++ balrogscript/src/balrogscript/balrog.py
@@ -2,7 +2,7 @@ import logging
 import pprint
 from copy import deepcopy
 
-from balrogclient import Rule, ScheduledRuleChange
+from balrogclient import API, Rule, ScheduledRuleChange
 from redo import retry
 
 # from requests.exceptions import HTTPError
@@ -10,11 +10,19 @@ from redo import retry
 log = logging.getLogger(__name__)
 
 
+def existing_api_data(api_obj, name):
+    """Call ``get_data`` on an API object and log."""
+    existing_data, existing_data_version = api_obj.get_data()  # XXX verify this works
+    log.info("%s data version %s:\n%s", name, existing_data_version, pprint.pformat(existing_data))
+    return existing_data, existing_data_version
+
+
 def update_existing_rule(rule_data, api_root, dummy, auth0_secrets):
     """Update an existing rule."""
-    # XXX should we pass in the Rule or ScheduledRuleChange object, for clean arch?
     rule_id = rule_data.pop("rule_id")  # XXX better error than a KeyError?
     scheduled_change = rule_data.pop("scheduledChange", False)
+    # XXX any verification here?
+    #     only support allowlisted products/channels?
     if scheduled_change:
         # XXX sc_rule_body ?
         rule_obj = ScheduledRuleChange(api_root=api_root, auth0_secrets=auth0_secrets, rule_id=rule_id)
@@ -25,9 +33,7 @@ def update_existing_rule(rule_data, api_root, dummy, auth0_secrets):
         rule_obj = Rule(api_root=api_root, auth0_secrets=auth0_secrets, rule_id=rule_id)
         update_fn = rule_obj.update_rule
         update_str = "Updating"
-    existing_data, existing_data_version = Rule.get_data()  # XXX verify this works
-    log.info("Rule %s data version %s:\n%s", rule_id, existing_data_version, pprint.pformat(existing_data))
-    # XXX fallbackMapping support?
+    existing_api_data(rule_obj, f"Rule {rule_id}")
     log.info("%s rule %s with data\n%s", update_str, rule_id, pprint.pformat(rule_data))
     retry(update_fn, kwargs=rule_data, sleeptime=2, max_sleeptime=2, attempts=10)
 
@@ -35,23 +41,37 @@ def update_existing_rule(rule_data, api_root, dummy, auth0_secrets):
 def create_rule(rule_data, api_root, dummy, auth0_secrets):
     """Create a rule that doesn't exist."""
     # XXX Scheduled Change support?
-    # XXX fallbackMapping support?
+    # XXX any verification here?
+    #     only support allowlisted products/channels?
     raise NotImplementedError("create_rule not written yet")
 
 
 def delete_existing_rule(rule_data, api_root, dummy, auth0_secrets):
-    """"""
-    # XXX Scheduled Change support?
+    """Delete an existing rule."""
     # http://mozilla-balrog.readthedocs.io/en/latest/admin_api.html#delete
-    raise NotImplementedError("delete_existing_rule not written yet")
+    # XXX any verification here?
+    #     only support allowlisted products/channels?
+    scheduled_change = rule_data.pop("scheduledChange", False)
+    if scheduled_change:
+        # XXX sc_rule_body ?
+        rule_obj = ScheduledRuleChange(api_root=api_root, auth0_secrets=auth0_secrets, rule_id=rule_id)
+        rule_data["change_type"] = "delete"
+        update_fn = rule_obj.add_scheduled_rule_change
+        update_str = "Scheduling deletion of"
+    else:
+        rule_obj = Rule(api_root=api_root, auth0_secrets=auth0_secrets, rule_id=rule_id)
+        rule_data["method"] = "DELETE"
+        update_fn = rule_obj.request
+        update_str = "Deleting"
+    existing_api_data(rule_obj, f"Rule {rule_id}")
+    log.info("%s rule %s with data\n%s",update_str, rule_id, pprint.pformat(rule_data))
+    retry(update_fn, kwargs=rule_data, sleeptime=2, max_sleeptime=2, attempts=10)
 
 
 def update_rules(rules, api_root, dummy, auth0_secrets):
     """"""
     for rule in rules:
         rule_data = deepcopy(rule)
-        # XXX any verification here?
-        #     only support allowlisted products/channels?
         delete_rule = rule_data.pop("deleteRule", False)
         if rule_data.pop("existingRule", True):
             if delete_rule:
