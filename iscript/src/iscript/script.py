f16bc4e685cb8d0d863ce80bf3a2444f710d6887
Geoff Brown <gbrown@mozilla.com>
2022-07-14 11:46:01 -0600

    skip notarize for geckodriver for now

--- iscript/src/iscript/script.py
+++ iscript/src/iscript/script.py
@@ -55,7 +55,7 @@ def get_behavior_function(behavior):
 
     """
     functions = {
-        "mac_geckodriver": (single_file_behavior, {"notarize": True}),
+        "mac_geckodriver": (single_file_behavior, {"notarize": False}),
         "mac_single_file": (single_file_behavior, {"notarize": True}),
         "mac_notarize": (notarize_behavior, {}),
         "mac_notarize_vpn": (vpn_behavior, {"notarize": True}),
