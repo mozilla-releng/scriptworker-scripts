c28b6e2df6b0fd36e024caba4f721b75fd8a5320
Julien Cristau <jcristau@mozilla.com>
2022-11-16 15:09:10 +0100

    Revert "Bug 1769081 - Enable authenticode_add_digicert_cross for firefox (#511)"
    This reverts commit 4baf7019a4a1623e907b02c8efe37c2321eb9071.

--- signingscript/docker.d/init_worker.sh
+++ signingscript/docker.d/init_worker.sh
@@ -52,7 +52,6 @@ case $COT_PRODUCT in
     test_var_set 'WIDEVINE_CERT'
 
     echo $WIDEVINE_CERT | base64 -d > $WIDEVINE_CERT_PATH
-    export AUTHENTICODE_ADD_DIGICERT_CROSS=1
     ;;
   thunderbird)
     ;;
