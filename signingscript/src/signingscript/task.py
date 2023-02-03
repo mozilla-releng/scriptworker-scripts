2282d341fcf4ade89710cd27ab63c810844240fa
Andrew Halberstadt <ahal@mozilla.com>
2023-02-03 11:34:23 -0500

    [RELENG-1011] Use proper signing format for authenticode EV signing

--- signingscript/src/signingscript/task.py
+++ signingscript/src/signingscript/task.py
@@ -53,6 +53,7 @@ FORMAT_TO_SIGNING_FUNCTION = immutabledict(
         "autograph_authenticode_sha2": sign_authenticode,
         "autograph_authenticode_sha2_stub": sign_authenticode,
         "autograph_authenticode_sha2_rfc3161_stub": sign_authenticode,
+        "autograph_authenticode_ev": sign_authenticode,
         "privileged_webextension": sign_xpi,
         "system_addon": sign_xpi,
         "autograph_rsa": sign_file_detached,
