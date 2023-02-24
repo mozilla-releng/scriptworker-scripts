d14b29063c5d36e32318a264b2520a51e8fe7ea6
Julien Cristau <jcristau@mozilla.com>
2023-02-24 10:39:35 +0100

    addonscript: add test coverage for addonscript.script
    Before:
    ```
    Name                            Stmts   Miss Branch BrPart  Cover   Missing
    ---------------------------------------------------------------------------
    src/addonscript/script.py          49     30     12      0    31%   19, 23-29, 36-54, 58-62, 74-88, 92
    ```
    
    After:
    ```
    Name                            Stmts   Miss Branch BrPart  Cover   Missing
    ---------------------------------------------------------------------------
    src/addonscript/script.py          49     16     12      0    61%   19, 23-29, 74-88, 92
    ```

--- addonscript/tests/conftest.py
+++ addonscript/tests/conftest.py
@@ -2,8 +2,21 @@ import tempfile
 
 import pytest
 
+from scriptworker.context import Context
 
 @pytest.fixture(scope="function")
 def tmpdir():
     with tempfile.TemporaryDirectory() as tmp:
         yield tmp
+
+
+@pytest.fixture(scope="function")
+def context():
+    context = Context()
+    context.config = {
+        "amo_instances": {
+            "project:releng:addons.mozilla.org:server:dev": {"amo_server": "http://some-amo-it.url", "jwt_user": "test-user", "jwt_secret": "secret"}
+        }
+    }
+    context.task = {"scopes": ["project:releng:addons.mozilla.org:server:dev"]}
+    return context
---
+++ addonscript/tests/test_script.py
@@ -0,0 +1,51 @@
+import os
+from unittest import mock
+
+from aiohttp.client_exceptions import ClientConnectionError
+import pytest
+import scriptworker.utils
+
+import addonscript.script as script
+from addonscript.exceptions import AMOConflictError, SignatureError
+
+@pytest.mark.asyncio
+async def test_sign_addon(context, mocker, tmpdir):
+    context.config["artifact_dir"] = tmpdir
+    mocker.patch.object(scriptworker.utils, "_define_sleep_time", return_value=0)
+    mocker.patch.object(script, "do_upload", side_effect=[ClientConnectionError(), {"pk": "pk"}])
+    mocker.patch.object(script, "get_signed_addon_url", side_effect=[SignatureError(), "http://download/this/en-GB.xpi"])
+    mocker.patch.object(script, "get_signed_xpi", side_effect=[SignatureError(), None])
+
+    await script.sign_addon(context, "en-GB")
+
+
+@pytest.mark.asyncio
+async def test_sign_addon_conflict(context, mocker, tmpdir):
+    context.config["artifact_dir"] = tmpdir
+    mocker.patch.object(scriptworker.utils, "_define_sleep_time", return_value=0)
+    mocker.patch.object(script, "do_upload", side_effect=[ClientConnectionError(), AMOConflictError("")])
+    mocker.patch.object(script, "get_signed_addon_url", side_effect=[SignatureError(), "http://download/this/en-GB.xpi"])
+    mocker.patch.object(script, "get_signed_xpi", side_effect=[SignatureError(), None])
+
+    await script.sign_addon(context, "en-GB")
+
+
+def test_build_locales_context(context, mocker):
+
+    def mock_get_langpack_info(filename):
+        locale = os.path.splitext(filename)[0]
+        return {
+            "id": f"langpack-{locale}@firefox.mozilla.org",
+            "locale": os.path.splitext(filename)[0],
+            "version": "110.0buildid20230224110500",
+            "unsigned": filename,
+            "min_version": "110.0",
+        }
+
+    mocker.patch.object(script, "get_langpack_info", side_effect=mock_get_langpack_info)
+    mocker.patch.object(script, "build_filelist", return_value=["en-US.xpi", "fr.xpi"])
+
+    script.build_locales_context(context)
+    assert list(context.locales) == ["en-US", "fr"]
+    for l in context.locales:
+        assert set(context.locales[l]) >= {"id", "min_version", "unsigned", "version"}
