0409930bf78ecddb277da4aabfb723a0c2794efe
Heitor Neiva <hneiva@mozilla.com>
2024-02-28 13:15:09 -0800

    feat(ci)!: Refactor script docker images and upload with skopeo (#907)
    * feat(ci)!: Build addonscript image with skopeo
    
    * feat(ci)!: Add other scripts to skopeo build
    
    * refactor(ci)!: Create a common script Dockerfile + Minor PR changes
    
    * refactor(ci): Added dockerignore and docker.d files to script images
    
    * ci: Use python_version template in tox kind instead of hard-coded version
    
    * ci!: Remove k8s-image kind and make push-image production-ready
    
    * chore: Move bitrise script to skopeo builds

---
+++ taskcluster/docker/skopeo/push_image.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+set -e
+
+test $APP
+test $DOCKER_TAG
+test $DOCKER_ARCHIVE_TAG
+test $DOCKER_REPO
+test $MOZ_FETCHES_DIR
+test $TASKCLUSTER_ROOT_URL
+test $TASK_ID
+test $VCS_HEAD_REPOSITORY
+test $VCS_HEAD_REV
+
+echo "=== Generating dockercfg ==="
+PASSWORD_URL=http://taskcluster/secrets/v1/secret/project/releng/scriptworker-scripts/deploy
+install -m 600 /dev/null $HOME/.dockercfg
+curl $PASSWORD_URL | jq '.secret.dockercfg' > $HOME/.dockercfg
+
+cd $MOZ_FETCHES_DIR
+unzstd image.tar.zst
+
+echo "=== Inserting version.json into image ==="
+# Create an OCI copy of image in order umoci can patch it
+skopeo copy docker-archive:image.tar oci:${APP}:final
+
+cat > version.json <<EOF
+{
+    "commit": "${VCS_HEAD_REV}",
+    "source": "${VCS_HEAD_REPOSITORY}",
+    "build": "${TASKCLUSTER_ROOT_URL}/tasks/${TASK_ID}"
+}
+EOF
+
+umoci insert --image ${APP}:final version.json /app/version.json
+
+echo "=== Pushing to docker hub ==="
+skopeo copy oci:${APP}:final docker://$DOCKER_REPO:$DOCKER_TAG
+skopeo copy oci:${APP}:final docker://$DOCKER_REPO:$DOCKER_ARCHIVE_TAG
+skopeo inspect docker://$DOCKER_REPO:$DOCKER_TAG
+
+echo "=== Clean up ==="
+rm -rf $HOME/.docker
