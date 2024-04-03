79ee97e057999fbf2845a2114e2846af3c997ae5
Julien Cristau <jcristau@mozilla.com>
2024-04-03 14:06:24 +0200

    Bug 1889336 - signingscript: add missing check for authenticode sha2 vars (#959)

--- signingscript/docker.d/init_worker.sh
+++ signingscript/docker.d/init_worker.sh
@@ -148,6 +148,8 @@ case $ENV in
       mozillavpn)
         test_var_set 'AUTOGRAPH_AUTHENTICODE_PASSWORD'
         test_var_set 'AUTOGRAPH_AUTHENTICODE_USERNAME'
+        test_var_set 'AUTOGRAPH_AUTHENTICODE_SHA2_PASSWORD'
+        test_var_set 'AUTOGRAPH_AUTHENTICODE_SHA2_USERNAME'
         test_var_set 'AUTHENTICODE_CERT_PATH'
         test_var_set 'AUTHENTICODE_CA_PATH'
         test_var_set 'AUTHENTICODE_CA_TIMESTAMP_PATH'
