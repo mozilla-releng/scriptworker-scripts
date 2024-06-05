0f9075e50cb5ea68c5b8b4d6cb0c77013e289a2e
Ben Hearsum <ben@mozilla.com>
2024-06-05 10:07:22 -0400

    feat(beetmoverscript): add support for globs in upstream artifacts

--- beetmoverscript/src/beetmoverscript/script.py
+++ beetmoverscript/src/beetmoverscript/script.py
@@ -2,11 +2,15 @@
 """Beetmover script"""
 
 import asyncio
+import copy
+import fnmatch
 import logging
 import mimetypes
 import os
+import os.path
 import re
 import sys
+from collections import defaultdict
 from multiprocessing.pool import ThreadPool
 
 import aiohttp
@@ -271,6 +275,123 @@ async def push_to_maven(context):
         context.artifacts_to_beetmove,
         artifact_map=context.task["payload"]["artifactMap"],
     )
+
+
+# TODO: maybe call upstreamArtifactPaths something different?
+def get_concrete_artifact_map_from_globbed(upstreamArtifactPaths, artifactMap, strip_prefixes = ["public/build", "public/logs"]):
+    # Sanity check inputs. Each file in upstreamArtifactPaths should match either:
+    # - One non "*" glob in artifactMap
+    # - A non "*" glob and "*" (in which case the former takes precedence)
+    # - "*" only
+    # TODO: maybe move this sanity check out elsewhere?
+    # Additionally, each destination should only have one matching artifact
+    # (ie: nothing should be overridden)
+
+    # upstreamArtifactPaths here is in the form of:
+    # {
+    #   "taskId1": [
+    #     "/path/to/file",
+    #     "/path/to/file2",
+    #   ],
+    #   "taskId2": [
+    #     "/path/to/file",
+    #     "/path/to/file2",
+    #   ],
+    # }
+    # artifactMap is in the form of:
+    # [
+    #   {
+    #     "taskId": "taskId1",
+    #     "paths": {
+    #       "*": {
+    #         "destinations": [
+    #           "dest1",
+    #         ]
+    #       }
+    #     },
+    #   }
+    # ]
+
+    concreteArtifactMap = []
+    errors = []
+
+    for map_ in artifactMap:
+        concretePaths = {}
+
+        full_glob_destinations = map_["paths"].get("*", {}).get("destinations")
+        other_paths = copy.deepcopy(map_["paths"])
+        if "*" in other_paths:
+            del other_paths["*"]
+
+        for taskId, artifacts in upstreamArtifactPaths.items():
+            if map_["taskId"] != taskId:
+                continue
+
+            for artifact in artifacts:
+                retained_artifact_path = artifact
+                for sp in strip_prefixes:
+                    if sp in artifact:
+                        retained_artifact_path = retained_artifact_path[retained_artifact_path.find(sp) + len(sp):]
+                destinations = []
+                # We need to look at non-'*' paths separate from '*' paths.
+
+                for input_path, output in other_paths.items():
+                    # Skip any input paths that don't match the artifact name.
+                    if "*" in input_path:
+                        if not fnmatch.fnmatch(retained_artifact_path, input_path):
+                            continue
+                    else:
+                        if input_path != artifact:
+                            continue
+
+                    # If there's already destinations, we've already seen this artifact,
+                    # and we have a clash.
+                    if destinations:
+                        errors.append(f"'{artifact}' matched multiple concrete paths")
+                    else:
+                        destinations.extend(output["destinations"])
+
+                
+                # If we have destinations for the "*" glob any artifacts that
+                # aren't accounted for by any of the `other_paths` will go to
+                # the "*" destinations.
+                if full_glob_destinations and not destinations:
+                    destinations.extend(full_glob_destinations)
+                
+                if destinations:
+                    concretePaths[artifact] = {"destinations": []}
+                    for d in destinations:
+                        if not d.endswith(retained_artifact_path):
+                            d = os.path.normpath(f"{d}{retained_artifact_path}")
+                        concretePaths[artifact]["destinations"].append(d)
+
+        concreteArtifactMap.append({
+            "paths": concretePaths,
+            "taskId": map_["taskId"],
+        })
+
+    if errors:
+        raise ScriptWorkerTaskException(*errors)
+
+    return concreteArtifactMap
+
+
+def ensure_no_overwrites_in_artifact_map(artifactMap):
+    dest_counts = defaultdict(int)
+    for map_ in artifactMap:
+        for output_path in map_["paths"].values():
+            for dest in output_path["destinations"]:
+                dest_counts[dest] += 1
+
+    errors = []
+    for dest, count in dest_counts.items():
+        if count > 1:
+            errors.append(f"'{dest}' would be written to more than once")
+
+    if errors:
+        raise ScriptWorkerTaskException(*errors)
+
+    return False
 
 
 # copy_beets {{{1
