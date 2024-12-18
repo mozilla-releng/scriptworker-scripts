1adee96406e5f72e4ec8b3963554046fd96868d7
Heitor Neiva <hneiva@mozilla.com>
2024-12-18 13:24:57 -0800

    feat(pushmsix): Bug 1931409 - Add TB to pushmsix

--- pushmsixscript/docker.d/init_worker.sh
+++ pushmsixscript/docker.d/init_worker.sh
@@ -21,23 +21,46 @@ export RELEASE_ROLLOUT_PERCENTAGE=25.0
 
 case $ENV in
   dev|fake-prod)
-    ;;
+      case $COT_PRODUCT in
+        # dev and fake-prod do not have variables set
+        firefox|thunderbird)
+          ;;
+        *)
+          echo "FAIL: Unsupported product in dev environment: ${COT_PRODUCT}" >&2
+          exit 1
+          ;;
+      esac
+      ;;
   prod)
-    test_var_set 'TENANT_ID'
-    test_var_set 'CLIENT_ID'
-    test_var_set 'CLIENT_SECRET'
-    export TENANT_ID=`echo $TENANT_ID | base64 -d`
-    export CLIENT_ID=`echo $CLIENT_ID | base64 -d`
-    export CLIENT_SECRET=`echo $CLIENT_SECRET | base64 -d`
+    case $COT_PRODUCT in
+      firefox)
+        # For firefox prod environment
+        test_var_set 'TENANT_ID'
+        test_var_set 'CLIENT_ID'
+        test_var_set 'CLIENT_SECRET'
+        export TENANT_ID=$(echo "$TENANT_ID" | base64 -d)
+        export CLIENT_ID=$(echo "$CLIENT_ID" | base64 -d)
+        export CLIENT_SECRET=$(echo "$CLIENT_SECRET" | base64 -d)
+        ;;
+      thunderbird)
+        # For thunderbird prod environment
+        test_var_set 'TB_TENANT_ID'
+        test_var_set 'TB_CLIENT_ID'
+        test_var_set 'TB_CLIENT_SECRET'
+        export TENANT_ID=$(echo "$COMM_TENANT_ID" | base64 -d)
+        export CLIENT_ID=$(echo "$COMM_CLIENT_ID" | base64 -d)
+        export CLIENT_SECRET=$(echo "$COMM_CLIENT_SECRET" | base64 -d)
+        ;;
+      *)
+        # Unsupported product in prod environment
+        echo "FAIL: Unsupported product in prod environment: ${COT_PRODUCT}" >&2
+        exit 1
+        ;;
+    esac
     ;;
   *)
-    exit 1
-    ;;
-esac
-
-case $COT_PRODUCT in
-  firefox) ;;
-  *)
+    # Unsupported environment
+    echo "FAIL: Unsupported environment: ${ENV}" >&2
     exit 1
     ;;
 esac
