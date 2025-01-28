a37d6b6d7dcf4df2af0892120526f77fadd7aa28
Bastien Orivel <borivel@mozilla.com>
2025-01-28 15:55:10 +0100

    Bump dependencies

--- maintenance/pin.sh
+++ maintenance/pin.sh
@@ -76,9 +76,9 @@ done
 
 if [ ${#PY38_DIRS} -gt 0 ]; then
     printf "FROM python:3.8\n${RUNCMD}" | docker build --platform linux/x86_64 --pull --tag "scriptworker-script-pin:3.8" -
-    echo "${PY38_DIRS[@]}" | xargs -n4 -P4 time docker run --platform linux/x86_64 --rm -t -v "$PWD":/src -e EXTRA_ARGS="$EXTRA_ARGS" -e SUFFIX=py38.txt -w /src scriptworker-script-pin:3.8 maintenance/pin-helper.sh
+    echo "${PY38_DIRS[@]}" | xargs -n4 -P4 docker run --platform linux/x86_64 --rm -t -v "$PWD":/src -e EXTRA_ARGS="$EXTRA_ARGS" -e SUFFIX=py38.txt -w /src scriptworker-script-pin:3.8 maintenance/pin-helper.sh
 fi
 if [ ${#PY311_DIRS} -gt 0 ]; then
     printf "FROM python:3.11\n${RUNCMD}" | docker build --platform linux/x86_64 --pull --tag "scriptworker-script-pin:3.11" -
-    echo "${PY311_DIRS[@]}" | xargs -n4 -P4 time docker run --platform linux/x86_64 --rm -t -v "$PWD":/src -e EXTRA_ARGS="$EXTRA_ARGS" -w /src scriptworker-script-pin:3.11 maintenance/pin-helper.sh
+    echo "${PY311_DIRS[@]}" | xargs -n4 -P4 docker run --platform linux/x86_64 --rm -t -v "$PWD":/src -e EXTRA_ARGS="$EXTRA_ARGS" -w /src scriptworker-script-pin:3.11 maintenance/pin-helper.sh
 fi
