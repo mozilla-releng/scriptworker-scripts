44cf6a31d204931e95605042ff46a3b0758198c8
Ben Hearsum <ben@mozilla.com>
2025-04-25 19:51:59 -0400

    fix(landoscript): add poll_time to config

--- landoscript/src/landoscript/script.py
+++ landoscript/src/landoscript/script.py
@@ -19,6 +19,7 @@ def get_default_config(base_dir: str = "") -> dict:
         "work_dir": os.path.join(base_dir, "work_dir"),
         "artifact_dir": os.path.join(base_dir, "artifact_dir"),
         "schema_file": os.path.join(os.path.dirname(__file__), "data", "landoscript_task_schema.json"),
+        "poll_time": 30,
     }
     return default_config
 
