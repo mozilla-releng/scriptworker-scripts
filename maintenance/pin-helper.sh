1c69557384238b1da06d952e3ac010083f4969bb
Heitor Neiva <hneiva@mozilla.com>
2025-05-01 09:25:52 -0700

    Bug 1963695 - Use uv for pinning dependencies + repin (#1195)
    - Removes use of docker + pip-compile-multi
    - auto-installs required python version with uv command line

--- maintenance/pin-helper.sh
+++ maintenance/pin-helper.sh
@@ -2,28 +2,55 @@
 # This runs in docker to pin our requirements files.
 set -e
 SUFFIX=${SUFFIX:-txt}
-
-# Supress pip root user warning
-export PIP_ROOT_USER_ACTION=ignore
+PYTHON_VERSION=${PYTHON_VERSION:-3.11}
+EXTRA_ARGS=${EXTRA_ARGS:-""}
 
 if [ $# -gt 0 ]; then
-    DIRS="$@"
+    read -r -a DIRS <<< "$@"
 else
     echo "Usage: pin-helper.sh [script(s)]"
     exit 1
 fi
 
-for dir in $DIRS; do
-    ARGS="$EXTRA_ARGS -g base -g test -g local"
-    if [ "$dir" = "pushflatpakscript" ]; then
-        ARGS="$ARGS -g flat-manager"
+for dir in "${DIRS[@]}"; do
+    if [ ! -d "$dir" ]; then
+        echo "Directory $dir does not exist"
+        exit 1
     fi
-    if [ "$dir" = "." ]; then
-        ARGS="$EXTRA_ARGS -g test -g docs"
+    if [ ! -d "$dir/requirements" ]; then
+        echo "Directory $dir/requirements does not exist"
+        exit 1
     fi
-    echo $ARGS
-    pushd "$dir"
-    pip-compile-multi --backtracking -u -o "$SUFFIX" $ARGS
-    chmod 644 requirements/*.txt || :
-    popd
+
+    pushd "$dir" > /dev/null
+
+    case "$dir" in
+        scriptworker_client)
+            REQ_FILES=(base test)
+            ;;
+        pushflatpakscript)
+            REQ_FILES=(base test local flat-manager)
+            ;;
+        .)
+            REQ_FILES=(test docs)
+            ;;
+        *)
+            REQ_FILES=(base test local)
+            ;;
+    esac
+
+    for req_file in "${REQ_FILES[@]}"; do
+        echo "$dir" "$req_file" "$SUFFIX"
+        if [ ! -f "requirements/$req_file.in" ]; then
+            echo "File $dir/requirements/$req_file.in does not exist"
+            exit 1
+        fi
+        uv pip compile "requirements/$req_file.in" \
+            --output-file "requirements/$req_file.$SUFFIX" \
+            --python-version "$PYTHON_VERSION" \
+            --universal \
+            --generate-hashes > /dev/null
+        chmod 644 "requirements/$req_file.$SUFFIX"
+    done
+    popd > /dev/null
 done
