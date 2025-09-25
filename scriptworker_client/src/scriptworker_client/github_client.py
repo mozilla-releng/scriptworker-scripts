9ef332763a3bb4e3f52bebbed1986efd5b5945bd
Ben Hearsum <ben@mozilla.com>
2025-09-25 11:19:40 -0400

    raise paths per query

--- scriptworker_client/src/scriptworker_client/github_client.py
+++ scriptworker_client/src/scriptworker_client/github_client.py
@@ -196,7 +196,7 @@ class GithubClient:
 
         return repo["object"]["oid"]
 
-    async def get_file_listing(self, paths: Union[str, List[str]] = "", branch: Optional[str] = None, depth_per_query=10, paths_per_query=1) -> List[str]:
+    async def get_file_listing(self, paths: Union[str, List[str]] = "", branch: Optional[str] = None, depth_per_query=5, paths_per_query=25) -> List[str]:
         """Get the recursive file and directory listings of the given path on
         the given branch, `depth_per_query` levels deep at a time.
 
