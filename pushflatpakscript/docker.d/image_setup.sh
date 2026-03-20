398bfdca0b9396b421de008f757dcb2ba5be4601
Julien Cristau <jcristau@mozilla.com>
2026-03-20 09:51:15 +0100

    flatpak: split dev and runtime dependencies (#1398)

--- pushflatpakscript/docker.d/image_setup.sh
+++ pushflatpakscript/docker.d/image_setup.sh
@@ -2,5 +2,5 @@
 set -o errexit -o pipefail
 
 apt-get update
-apt-get install --no-install-recommends -y curl gir1.2-ostree-1.0 libgirepository-2.0-dev ostree gcc libcairo2-dev
+apt-get install --no-install-recommends -y gir1.2-ostree-1.0 libgirepository-2.0-0 ostree
 apt-get clean
