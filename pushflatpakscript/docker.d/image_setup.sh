0ff224f46228d2ec9cbcacc9f1f629ebc8401e05
Julien Cristau <jcristau@mozilla.com>
2026-05-27 16:37:10 +0200

    pushflatpak: update flat-manager-client (bug 2042414)
    Use the new rust implementation of flat-manager-client, the old python
    one is incompatible with some recent changes to flat-manager.

--- pushflatpakscript/docker.d/image_setup.sh
+++ pushflatpakscript/docker.d/image_setup.sh
@@ -2,5 +2,5 @@
 set -o errexit -o pipefail
 
 apt-get update
-apt-get install --no-install-recommends -y gir1.2-ostree-1.0 libgirepository-2.0-0 ostree
+apt-get install --no-install-recommends -y ostree
 apt-get clean
