c5a64be485304e0e156c5d3f1c592e2ac35b2e4e
Julien Cristau <jcristau@mozilla.com>
2026-06-02 14:35:11 +0200

    init: don't require a COT_PRODUCT (bug 2006684)

--- builddecisionscript/docker.d/init_worker.sh
+++ builddecisionscript/docker.d/init_worker.sh
@@ -13,7 +13,3 @@ test_var_set() {
 test_var_set 'TASKCLUSTER_ROOT_URL'
 
 export VERIFY_CHAIN_OF_TRUST=false
-
-export WORKER_TYPE="${PROJECT_NAME}${WORKER_SUFFIX}"
-export WORKER_GROUP=${WORKER_TYPE}
-export WORKER_ID_PREFIX="${WORKER_TYPE}-"
