cc6f3f9a20a9bc52144bb3219334ecbf6c106ad1
abhishekmadan30 <amadan@mozilla.com>
2026-06-25 17:49:45 -0400

    feat!: add support to publish to huawei store

--- pushapkscript/tests/integration/test_integration_script.py
+++ pushapkscript/tests/integration/test_integration_script.py
@@ -206,6 +206,9 @@ class ConfigFileGenerator(object):
                                     "service_account_id": "123",
                                     "access_token": "456",
                                 },
+                                "huawei": {
+                                    "credentials_file": "huawei.json",
+                                },
                             },
                         },
                     }
@@ -287,6 +290,7 @@ class MainTest(unittest.TestCase):
             skip_checks_fennec=False,
             sgs_service_account_id=None,
             sgs_access_token=None,
+            huawei_credentials=None,
             submit=False,
         )
 
@@ -316,6 +320,7 @@ class MainTest(unittest.TestCase):
             skip_checks_fennec=True,
             sgs_service_account_id=None,
             sgs_access_token=None,
+            huawei_credentials=None,
             submit=False,
         )
 
@@ -345,6 +350,7 @@ class MainTest(unittest.TestCase):
             skip_checks_fennec=True,
             sgs_service_account_id=None,
             sgs_access_token=None,
+            huawei_credentials=None,
             submit=False,
         )
 
@@ -374,6 +380,7 @@ class MainTest(unittest.TestCase):
             skip_checks_fennec=False,
             sgs_service_account_id=None,
             sgs_access_token=None,
+            huawei_credentials=None,
             submit=False,
         )
 
@@ -403,6 +410,7 @@ class MainTest(unittest.TestCase):
             skip_checks_fennec=False,
             sgs_service_account_id=None,
             sgs_access_token=None,
+            huawei_credentials=None,
             submit=False,
         )
 
@@ -433,6 +441,7 @@ class MainTest(unittest.TestCase):
             skip_checks_fennec=False,
             sgs_service_account_id=None,
             sgs_access_token=None,
+            huawei_credentials=None,
             submit=False,
         )
 
@@ -463,6 +472,38 @@ class MainTest(unittest.TestCase):
             skip_checks_fennec=True,
             sgs_service_account_id="123",
             sgs_access_token="456",
+            huawei_credentials=None,
+            submit=False,
+        )
+
+    @unittest.mock.patch("pushapkscript.publish.push_apk")
+    def test_main_with_huawei_store(self, push_apk):
+        task_generator = TaskGenerator(should_commit_transaction=True, store="huawei")
+
+        self.write_task_file(task_generator.generate_task("fenix", channel="release"))
+
+        self._prepare_apks(task_generator, "fenix-production")
+        main(config_path=self.config_generator.generate_fenix_config())
+
+        push_apk.assert_called_with(
+            apks=[
+                MockFile("{}/work/cot/{}/public/build/target.apk".format(self.test_temp_dir, task_generator.arm_task_id)),
+                MockFile("{}/work/cot/{}/public/build/target.apk".format(self.test_temp_dir, task_generator.x86_task_id)),
+            ],
+            secret=None,
+            track=None,
+            expected_package_names=["org.mozilla.fenix"],
+            store="huawei",
+            rollout_percentage=None,
+            dry_run=False,
+            contact_server=True,
+            skip_check_multiple_locales=True,
+            skip_check_ordered_version_codes=False,
+            skip_check_same_locales=True,
+            skip_checks_fennec=True,
+            sgs_service_account_id=None,
+            sgs_access_token=None,
+            huawei_credentials="huawei.json",
             submit=False,
         )
 
