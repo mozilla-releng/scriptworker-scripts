52d335090c2214b440b88165d31f67e1d8566472
Ben Hearsum <ben@mozilla.com>
2026-07-16 14:26:23 -0400

    support direct git tags in landoscript

--- landoscript/tests/test_tag.py
+++ landoscript/tests/test_tag.py
@@ -59,6 +59,50 @@ async def test_success(aioresponses, github_installation_responses, context, tag
 
 
 @pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "tag_info,dry_run",
+    (
+        pytest.param(
+            {
+                "revision": "ghijkl654321",
+                "tags": ["BUILD1"],
+            },
+            True,
+            id="dry_run",
+        ),
+        pytest.param(
+            {
+                "revision": "ghijkl654321",
+                "tags": ["BUILD1"],
+            },
+            False,
+            id="one_tag",
+        ),
+        pytest.param(
+            {
+                "revision": "ghijkl654321",
+                "tags": ["BUILD1", "RELEASE"],
+            },
+            False,
+            id="multiple_tags",
+        ),
+    ),
+)
+async def test_success_git(aioresponses, github_installation_responses, context, tag_info, dry_run):
+    payload = {
+        "actions": ["tag"],
+        "lando_repo": "repo_name",
+        "tag_info": tag_info,
+        "dry_run": dry_run,
+    }
+
+    def assert_func(req):
+        assert_tag_response(req, tag_info, tag_info["revision"])
+
+    await run_test(aioresponses, github_installation_responses, context, payload, ["tag"], not dry_run, assert_func)
+
+
+@pytest.mark.asyncio
 async def test_no_tags(aioresponses, github_installation_responses, context):
     payload = {
         "actions": ["tag"],
@@ -70,19 +114,6 @@ async def test_no_tags(aioresponses, github_installation_responses, context):
         },
     }
     await run_test(aioresponses, github_installation_responses, context, payload, ["tag"], err=TaskVerificationError, errmsg="must provide at least one tag!")
-
-
-@pytest.mark.asyncio
-async def test_hg_repo_url(aioresponses, github_installation_responses, context):
-    payload = {
-        "actions": ["tag"],
-        "lando_repo": "repo_name",
-        "tag_info": {
-            "revision": "abcdef123456",
-            "tags": ["FOO"],
-        },
-    }
-    await run_test(aioresponses, github_installation_responses, context, payload, ["tag"], err=TaskVerificationError, errmsg="must provide hg_repo_url!")
 
 
 @pytest.mark.parametrize(
