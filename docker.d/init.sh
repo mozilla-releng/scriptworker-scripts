f6c2b02209e63b14cda42f40cee49a6abdd01fa5
Julien Cristau <jcristau@mozilla.com>
2026-09-25 17:08:31 +0200

    Bug 2075583 - require GITHUB_OAUTH_TOKEN for firefox scriptworkers (#1529)
    With release-graph tasks now cloning the firefox git repo, chain of
    trust needs to make github API calls.

--- docker.d/init.sh
+++ docker.d/init.sh
@@ -25,7 +25,7 @@ if [ "$ENV" == "prod" ]; then
   test_var_set 'ED25519_PRIVKEY'
 fi
 case $COT_PRODUCT in
-  firefox|thunderbird)
+  thunderbird)
     ;;
   *)
     test_var_set 'GITHUB_OAUTH_TOKEN'
